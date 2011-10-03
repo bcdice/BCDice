4469169b2ea3c1c412cc1d3611add454b03a7799
torgtaitai <master@torgtaitai.com>
2011-10-03 22:56:15 +0900

    EclipsePhase.rbの追加忘れ。

---
+++ src/diceBot/EclipsePhase.rb
@@ -0,0 +1,48 @@
+#--*-coding:utf-8-*--
+
+class EclipsePhase < DiceBot
+  
+  def initialize
+    super
+  end
+  
+  def gameType
+    "EclipsePhase"
+  end
+  
+  def getHelpMessage
+    return <<MESSAGETEXT
+1d100判定時にクリティカル・ファンブル自動判定
+MESSAGETEXT
+  end
+  
+  def check_1D100(total_n, dice_n, signOfInequality, diff, dice_cnt, dice_max, n1, n_max)    # ゲーム別成功度判定(1d100)
+    return '' unless( signOfInequality == '<=' )
+    
+    diceValue = total_n % 100 #これで１〜１００の値を０〜９９にする。
+    dice0 = diceValue / 10 #10の位を代入
+    dice1 = diceValue % 10 # 1の位を代入
+    
+    debug("total_n", total_n)
+    debug("dice0, dice1", dice0, dice1)
+    
+    if( dice0 != dice1 )
+      if(total_n <= diff)
+        return ' ＞ 成功'
+        else
+        return ' ＞ 失敗'
+      end
+    end
+    
+    if( (dice0 == 0) and (dice1 == 0) )
+      return ' ＞ 00 ＞ クリティカル'
+    end
+    
+    if(total_n <= diff)
+      return ' ＞ クリティカル'
+    else
+      return ' ＞ ファンブル'
+    end
+  end
+
+end
