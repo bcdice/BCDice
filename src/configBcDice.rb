a964ba2bd3dea838d53345c4571996d09cf7eebe
torgtaitai <master@torgtaitai.com>
2011-12-11 12:19:40 +0900

    バージョン表記を修正

--- src/configBcDice.rb
+++ src/configBcDice.rb
@@ -6,7 +6,7 @@ require 'configBcDiceForSystem.rb'
 $isDebug = false
 
 $KCODE = 'UTF8'# このソースはUTF-8で書かれています
-$version = "2.01.07"
+$version = "2.01.09"
 
 
 $NOTICE_SW = 1;                  # 送信の際に、どちらのコマンドを使うか？(notice=1, msg=0)
