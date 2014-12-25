b9353fc81c90cd949fee52da7ebed85b814e5c41
ocha <ochaochaocha3@gmail.com>
2014-12-25 17:53:26 +0900

    デバッグ出力の文字化けを修正
    文字コードをShift_JISに固定していたため、デバッグ出力がそれ以外の
    文字コードの端末で文字化けするのを修正。
    
    * Ruby 1.8（Windows）：従来と同じ処理を行う
    * Ruby 1.8（Windows以外）：文字コードを変換せず出力する
    * Ruby 1.9以降：Encoding.default_externalに依存するRubyの自動
        エンコード機構を利用する。文字コードを明示的に変換しない

--- src/log.rb
+++ src/log.rb
@@ -2,43 +2,32 @@
 
 $isDebug = false
 
-def debug(obj1, *obj2)
-  return unless( $isDebug )
-  
-  unless( obj1.is_a?(String) )
-    obj1 = obj1.inspect
-  end
-
-  if( obj2.empty? )
-    debugPrint("#{obj1}\n".tosjis)
-    return
-  end
-  
-  obj2 = getTextFromLogTarget(obj2)
-  
-  debugPrint("#{obj1} : #{obj2}\n".tosjis)
-end
-
+# デバッグ文字列出力（末尾改行なし）
 def debugPrint(text)
-  print(text)
+  print($RUBY18_WIN ? text.tosjis : text)
 end
 
-def getTextFromLogTarget(target)
-  return target.inspect if( target.nil? )
-  
-  list = target.collect do |i|
-    if( i.is_a?(String) )
-      '"' + i + '"'
-      #print(i + "\n")
-    else
-      i.inspect
+# デバッグ文字列出力（末尾改行あり）
+def debugPuts(text)
+  line = "#{text}\n"
+  puts($RUBY18_WIN ? line.tosjis : line)
+end
+
+# デバッグ出力を行う
+# @param [Object] target 対象項目
+# @param [Object] values 値
+def debug(target, *values)
+  return unless $isDebug
+
+  targetStr = target.kind_of?(String) ? target : target.inspect
+
+  if values.empty?
+    debugPuts(targetStr)
+  else
+    valueStrs = values.map do |value|
+      value.kind_of?(String) ? %Q("#{value}") : value.inspect
     end
+
+    debugPuts("#{targetStr}: #{valueStrs.join(', ')}")
   end
-  
-  return list.join(", ")
 end
-
-
-
-
-
