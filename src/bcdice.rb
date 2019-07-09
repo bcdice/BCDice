f7a04d601fd8e2d3f916c48a0b4981b231b4c002
ukatama <dev.ukatama@gmail.com>
2019-07-09 13:48:53 +0900

    文字列連結の<<を置換

--- src/bcdice.rb
+++ src/bcdice.rb
@@ -1,7 +1,7 @@
 #!/bin/ruby -Ku
 # -*- coding: utf-8 -*-
 
-$LOAD_PATH << File.dirname(__FILE__) # require_relative対策
+$LOAD_PATH = $LOAD_PATH + File.dirname(__FILE__) # require_relative対策
 
 require 'configBcDice.rb'
 
