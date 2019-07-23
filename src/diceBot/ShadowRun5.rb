a766743cd0cd94e247e99a3115f6f267e3ce8dda
saronpasu <jamneco@gmail.com>
2019-07-23 14:47:35 +0900

    add diceBots. ShadowRun5thEdition and NjslyrTrpg.

---
+++ src/diceBot/ShadowRun5.rb
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+require 'diceBot/ShadowRun4'
+
+class ShadowRun5 < ShadowRun4
+  setPrefixes(['(\d+)B6@(\d+)'])
+
+  def initialize
+    super
+    @sortType = 3
+    @rerollNumber = 6 # 振り足しする出目
+    @defaultSuccessTarget = ">=5" # 目標値が空欄の時の目標値
+  end
+
+  def gameName
+    'シャドウラン第５版'
+  end
+
+  def gameType
+    "ShadowRun5"
+  end
+
+  def getHelpMessage
+    return <<INFO_MESSAGE_TEXT
+個数振り足しロール(xRn)の境界値を6にセット、バラバラロール(xBn)の目標値を5以上にセットします。
+バラバラロール(xBn)のみ、リミットをセットできます。リミットの指定は(xBn@l)のように指定します。(省略可)
+BコマンドとRコマンド時に、グリッチの表示を行います。
+INFO_MESSAGE_TEXT
+  end
+
+  # シャドウラン5版　リミット時のテスト
+  def rollDiceCommand(command)
+    debug('chatch limit prefix')
+
+    command.match(/(\d+)B6@(\d+)/)
+    dicepool = Regexp.last_match(1).to_i
+    limit = Regexp.last_match(2).to_i
+    output_before_limited, secret = @@bcdice.checkBDice(dicepool.to_s + 'B6')
+    output_before_limited.match(/成功数(\d+)/)
+    output_after_limited = output_before_limited
+    before_suc_cnt = Regexp.last_match(1).to_i
+    after_suc_cnt = Regexp.last_match(1).to_i
+    over_suc_cnt = 0
+    if before_suc_cnt > limit
+      after_suc_cnt = limit
+      over_suc_cnt = before_suc_cnt - limit
+      output_after_limited = output_before_limited.gsub(/成功数(\d+)/, '成功数' + after_suc_cnt.to_s)
+      output_after_limited += '(リミット超過' + over_suc_cnt.to_s + ')'
+    end
+    output = output_after_limited
+    output = output.gsub(/^../, '')
+    output = output.gsub(/B/, 'B6')
+    output = output.gsub(/6>=5/, '[6]Limit[' + limit.to_s + ']>=5')
+    debug(output)
+    return output
+  end
+end
