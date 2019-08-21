7f3b8701d1e6f07b4db69cf4c1274d6dc8e11a0b
esnya <dev.ukatama@gmail.com>
2019-08-21 13:44:57 +0900

    テストデータ変換ツール追加

---
+++ src/test/txt2yml.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# txt形式のテストデータをYAMLに変換するスクリプト
+
+require 'yaml'
+require './DiceBotTestData'
+
+def listDataFiles
+  Dir.glob('data/*.txt')
+end
+
+def parseDataFile(filename)
+  gameType = File.basename(filename, '.txt')
+  tests = File.open(filename).read
+              .gsub("\r\n", "\n")
+              .tr("\r", "\n")
+              .split("============================\n")
+              .map do |testSource, index|
+    DiceBotTestData.parse(testSource.chomp, gameType, index)
+  end
+
+  return gameType, tests
+end
+
+def toHash(test)
+  {
+    'input' => test.input,
+    'output' => test.output,
+    'rands' => test.rands,
+  }
+end
+
+listDataFiles.map do |filename|
+  gameType, tests = parseDataFile(filename)
+  testHashList = tests.map do |test|
+    toHash(test)
+  end
+  content = YAML.dump('tests' => testHashList, 'gameType' => gameType)
+                .gsub(/^  - - ([0-9]+)\n    - ([0-9]+)$/) { "  - [#{Regexp.last_match(1)}, #{Regexp.last_match(2)}]" }
+  File.open(filename.gsub(/\.txt$/, '.yml'), 'w') do |file|
+    file.write(content)
+  end
+end
