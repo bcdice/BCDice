9e22310500884a78f10214a6010d031b976516b2
ocha <ochaochaocha3@gmail.com>
2019-08-22 04:29:05 +0900

    NinjaSlayer: 回避判定を簡略化する

--- src/diceBot/NinjaSlayer.rb
+++ src/diceBot/NinjaSlayer.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+require 'diceBot/DiceBot'
+
 class NinjaSlayer < DiceBot
   # ダイスボットで使用するコマンドを配列で列挙する
   setPrefixes([
@@ -29,7 +31,7 @@ class NinjaSlayer < DiceBot
 　x=判定ダイス y=難易度 省略時はNORMAL(4)
 　例:NJ4@H 難易度HARD、判定ダイス4で判定
 ・回避判定　EV
-　EVx[y]/z or EVx@y/z or EVx/z or EVx
+　EVx[y]/z or EVx@y/z or EVx/z or EVx[y] or EVx@y or EVx
 　x=判定ダイス y=難易度 z=攻撃側の成功数(省略可) 難易度を省略時はNORMAL(4)
 　攻撃側の成功数を指定した場合、カウンターカラテ発生時には表示
 　例:EV5/3 難易度NORMAL(省略時)、判定ダイス5、攻撃側の成功数3で判定
@@ -49,6 +51,11 @@ MESSAGETEXT
   DIFFICULTY_RE = /\[(#{DIFFICULTY_VALUE_RE})\]|@(#{DIFFICULTY_VALUE_RE})/io
   # 通常判定の正規表現
   NJ_RE = /\ANJ(\d+)#{DIFFICULTY_RE}?\z/io
+  # 回避判定の正規表現
+  EV_RE = %r{\AEV(\d+)#{DIFFICULTY_RE}?(?:/(\d+))?\z}io
+
+  # 回避判定のノード
+  EV = Struct.new(:num, :difficulty, :targetValue)
 
   # 難易度の文字表現から整数値への対応
   DIFFICULTY_SYMBOL_TO_INTEGER = {
@@ -63,21 +70,18 @@ MESSAGETEXT
     m = NJ_RE.match(str)
     return str unless m
 
-    return "#{m[1]}B6>=#{integerValueOfDifficulty(m[2] || m[3])}"
+    return bRollCommand(m[1], integerValueOfDifficulty(m[2] || m[3]))
   end
 
   def rollDiceCommand(command)
     debug('rollDiceCommand begin string', command)
 
-    result = ''
+    case node = parse(command)
+    when EV
+      return executeEV(node)
+    end
 
-    # ev roll difficults
-    ev_kids_pattern = %r{^(?:ev)(\d+)(?:@|\[)?[k2](?:\])?(?:/)?(\d+)?}i
-    ev_easy_pattern = %r{^(?:ev)(\d+)(?:@|\[)?[e3](?:\])?(?:/)?(\d+)?}i
-    ev_normal_pattern = %r{^(?:ev)(\d+)(?:@|\[)?[n4](?:\])?(?:/)?(\d+)?}i
-    ev_hard_pattern = %r{^(?:ev)(\d+)(?:@|\[)?[h5](?:\])?(?:/)?(\d+)?}i
-    ev_ultrahard_pattern = %r{^(?:ev)(\d+)(?:@|\[)?(?:uh|6)(?:\])?(?:/)?(\d+)?}i
-    ev_short_pattern = %r{^(?:ev)(\d+)(?:/)?(\d+)?$}i
+    result = ''
 
     # combat attack difficults
     at_kids_pattern = /^(?:at)(\d+)(?:@|\[)?[k2](?:\])?/i
@@ -87,125 +91,8 @@ MESSAGETEXT
     at_ultrahard_pattern = /^(?:at)(\d+)(?:@|\[)?(?:uh|6)(?:\])?/i
     at_short_pattern = /^(?:at)(\d+)$/i
 
-    # EV evaluate
-    if (m = ev_kids_pattern.match(command))
-      debug('ev kids: ', command)
-
-      dice_cnt = m[1]
-      suc_rate ||= m[2]
-      dice = roll(m[1], 6, 0, 0, '>=', 2)
-      dice_str = dice[1]
-      suc_cnt = dice[5]
-      counter_karate = false
-
-      if !suc_rate.nil? && suc_cnt > suc_rate.to_i
-        debug('counter karate: true')
-        counter_karate = true
-      end
-
-      result += "(#{m[1]}B6>=2) ＞ #{dice_str} ＞ 成功数#{suc_cnt}"
-      if counter_karate
-        result += ' ＞ カウンターカラテ!!'
-      end
-
-    elsif (m = ev_easy_pattern.match(command))
-      debug('ev easy: ', command)
-
-      dice_cnt = m[1]
-      suc_rate ||= m[2]
-      dice = roll(m[1], 6, 0, 0, '>=', 3)
-      dice_str = dice[1]
-      suc_cnt = dice[5]
-      counter_karate = false
-
-      if !suc_rate.nil? && suc_cnt > suc_rate.to_i
-        debug('counter karate: true')
-        counter_karate = true
-      end
-
-      result += "(#{m[1]}B6>=3) ＞ #{dice_str} ＞ 成功数#{suc_cnt}"
-      if counter_karate
-        result += ' ＞ カウンターカラテ!!'
-      end
-    elsif (m = ev_normal_pattern.match(command))
-      debug('ev normal: ', command)
-
-      dice_cnt = m[1]
-      suc_rate ||= m[2]
-      dice = roll(m[1], 6, 0, 0, '>=', 4)
-      dice_str = dice[1]
-      suc_cnt = dice[5]
-      counter_karate = false
-
-      if !suc_rate.nil? && suc_cnt > suc_rate.to_i
-        debug('counter karate: true')
-        counter_karate = true
-      end
-
-      result += "(#{m[1]}B6>=4) ＞ #{dice_str} ＞ 成功数#{suc_cnt}"
-      if counter_karate
-        result += ' ＞ カウンターカラテ!!'
-      end
-    elsif (m = ev_hard_pattern.match(command))
-      debug('ev hard: ', command)
-
-      dice_cnt = m[1]
-      suc_rate ||= m[2]
-      dice = roll(m[1], 6, 0, 0, '>=', 5)
-      dice_str = dice[1]
-      suc_cnt = dice[5]
-      counter_karate = false
-
-      if !suc_rate.nil? && suc_cnt > suc_rate.to_i
-        debug('counter karate: true')
-        counter_karate = true
-      end
-
-      result += "(#{m[1]}B6>=5) ＞ #{dice_str} ＞ 成功数#{suc_cnt}"
-      if counter_karate
-        result += ' ＞ カウンターカラテ!!'
-      end
-    elsif (m = ev_ultrahard_pattern.match(command))
-      debug('ev ultrahard: ', command)
-
-      dice_cnt = m[1]
-      suc_rate ||= m[2]
-      dice = roll(m[1], 6, 0, 0, '>=', 6)
-      dice_str = dice[1]
-      suc_cnt = dice[5]
-      counter_karate = false
-
-      if !suc_rate.nil? && suc_cnt > suc_rate.to_i
-        debug('counter karate: true')
-        counter_karate = true
-      end
-
-      result += "(#{m[1]}B6>=6) ＞ #{dice_str} ＞ 成功数#{suc_cnt}"
-      if counter_karate
-        result += ' ＞ カウンターカラテ!!'
-      end
-    elsif (m = ev_short_pattern.match(command))
-      debug('ev short: ', command)
-
-      dice_cnt = m[1]
-      suc_rate ||= m[2]
-      dice = roll(m[1], 6, 0, 0, '>=', 4)
-      dice_str = dice[1]
-      suc_cnt = dice[5]
-      counter_karate = false
-
-      if !suc_rate.nil? && suc_cnt > suc_rate.to_i
-        debug('counter karate: true')
-        counter_karate = true
-      end
-
-      result += "(#{m[1]}B6>=4) ＞ #{dice_str} ＞ 成功数#{suc_cnt}"
-      if counter_karate
-        result += ' ＞ カウンターカラテ!!'
-      end
-
     # AT evaluate
-    elsif (m = at_kids_pattern.match(command))
+    if (m = at_kids_pattern.match(command))
       debug('at kids: ', command)
 
       dice_cnt = m[1]
@@ -330,6 +217,51 @@ MESSAGETEXT
 
   private
 
+  # 構文解析する
+  # @param [String] command コマンド文字列
+  # @return [EV, nil]
+  def parse(command)
+    case
+    when mEV = EV_RE.match(command)
+      return parseEV(mEV)
+    else
+      return nil
+    end
+  end
+
+  # 正規表現のマッチ情報から回避判定ノードを作る
+  # @param [MatchData] m 正規表現のマッチ情報
+  # @return [EV]
+  def parseEV(m)
+    num = m[1].to_i
+    difficulty = integerValueOfDifficulty(m[2] || m[3])
+    targetValue = m[4] && m[4].to_i
+
+    return EV.new(num, difficulty, targetValue)
+  end
+
+  # 回避判定を行う
+  # @param [EV] ev 回避判定ノード
+  # @return [String] 回避判定結果
+  def executeEV(ev)
+    command = bRollCommand(ev.num, ev.difficulty)
+    rollResult = bcdice.
+      bdice(command).
+      sub(/\A[^(]+/, '')
+
+    return rollResult unless ev.targetValue
+
+    m = /成功数(\d+)/.match(rollResult)
+    raise '成功数が見つかりません' unless m
+    numOfSuccesses = m[1].to_i
+
+    if numOfSuccesses > ev.targetValue
+      return "#{rollResult} ＞ カウンターカラテ!!"
+    end
+
+    return rollResult
+  end
+
   # 難易度の整数値を返す
   # @param [String, nil] s 難易度表記
   # @return [Integer] 難易度の整数値
@@ -343,5 +275,13 @@ MESSAGETEXT
     return s.to_i if /\A[2-6]\z/.match(s)
 
     return DIFFICULTY_SYMBOL_TO_INTEGER.fetch(s.upcase)
+  end
+
+  # バラバラロールのコマンドを返す
+  # @param [#to_s] num ダイス数
+  # @param [#to_s] difficulty 難易度
+  # @return [String]
+  def bRollCommand(num, difficulty)
+    "#{num}B6>=#{difficulty}"
   end
 end
