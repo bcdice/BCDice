16616a0573e000ca5552a3792ac767144835a2f0
esnya <dev.ukatama@gmail.com>
2019-08-22 18:14:56 +0900

    [opal] Nil チェック

--- src/diceBot/MetalHeadExtream.rb
+++ src/diceBot/MetalHeadExtream.rb
@@ -906,7 +906,7 @@ MESSAGETEXT
   # 端数が使いたいので、parren_killer未使用
   def get_value(originalValue, calculateText)
     result = originalValue.to_f
-    calculateArray = calculateText.scan(%r{[\*/]\d*}) # 修正値を分割して配列へ
+    calculateArray = (calculateText || '').scan(%r{[\*/]\d*}) # 修正値を分割して配列へ
     calculateArray.each do |i| # 配列内ループで補正率を計算
       i =~ %r{([\*/])(\d*)}i
       result *= Regexp.last_match(2).to_i if Regexp.last_match(1) == '*'
