cbf9886e1f74b7bafc041852f21ec900143b2e28
criticality-psn <52457737+criticality-psn@users.noreply.github.com>
2020-03-25 17:20:07 +0900

    Change to use Table/D66GridTable

--- src/diceBot/StellarKnights.rb
+++ src/diceBot/StellarKnights.rb
@@ -75,92 +75,16 @@ MESSAGETEXT
       return table.roll(bcdice)
     elsif (m = /(\d+)SK(\d)?((,\d>\d)+)?/.match(command))
       return resolute_action(m[1].to_i, m[2] && m[2].to_i, m[3], command)
+    elsif command == 'ALLS'
+      return roll_all_situation_tables
     elsif command == "PET"
-      return roll_personality_table()
+      return roll_personality_table
     elsif (m = /FT(\d+)?/.match(command))
       num = (m[1] || 5).to_i
       return roll_fragment_table(num)
     end
 
     return analyzeDiceCommandResultMethod(command)
-  end
-
-  def getThemeTableDiceCommandResult(command)
-    return unless command == "TT"
-
-    tableName = "お題表"
-    table = %w{
-      未来 占い 遠雷 恋心 歯磨き 鏡
-      過去 キス ささやき声 黒い感情 だっこ 青空
-      童話 決意 風の音 愛情 寝顔 鎖
-      ふたりの秘密 アクシデント！ 小鳥の鳴き声 笑顔 食事 宝石
-      思い出 うとうと 鼓動 嫉妬 ベッド 泥
-      恋の話 デート ため息 内緒話 お風呂 小さな傷
-    }
-
-    text, index = get_table_by_d66(table)
-
-    result = "#{tableName}(#{index}) ＞ #{text}"
-    return result
-  end
-
-  def getSituationTableDiceCommandResult(command)
-    return unless command == "STA"
-
-    tableName = "シチュエーション表A：時間"
-    table = %w{
-      朝、誰もいない
-      騒がしい昼間の
-      寂しい夕暮れの横たわる
-      星の瞬く夜、
-      静謐の夜更けに包まれた
-      夜明け前の
-    }
-    text, index = get_table_by_1d6(table)
-
-    result = "#{tableName}(#{index}) ＞ #{text}"
-    return result
-  end
-
-  def getPlageTableDiceCommandResult(command)
-    return unless command == "STB"
-
-    tableName = "シチュエーション表B：場所"
-
-    table_1_2 = [
-      "教室 　小道具：窓、机、筆記用具、チョークと黒板、窓の外から聞こえる部活動の声",
-      "カフェテラス　小道具：珈琲、紅茶、お砂糖とミルク、こちらに手を振っている学友",
-      "学園の中庭　小道具：花壇、鳥籠めいたエクステリア、微かに聴こえる鳥の囁き",
-      "音楽室　小道具：楽器、楽譜、足踏みオルガン、壁に掛けられた音楽家の肖像画",
-      "図書館　小道具：高い天井、天井に迫る程の本棚、無数に収められた本",
-      "渡り廊下　小道具：空に届きそうな高さ、遠くに別の学園が見える、隣を飛び過ぎて行く鳥",
-    ]
-
-    table_3_4 = [
-      "花の咲き誇る温室　小道具：むせ返るような花の香り、咲き誇る花々、ガラス越しの陽光",
-      "アンティークショップ　小道具：アクセサリーから置物まで、見慣れない古い機械は地球時代のもの？",
-      "ショッピングモール　小道具：西欧の街並みを思わせるショッピングモール、衣類に食事、お茶屋さんも",
-      "モノレール　小道具：車窓から覗くアーセルトレイの街並み、乗客はあなたたちだけ",
-      "遊歩道　小道具：等間隔に並ぶ街路樹、レンガ造りの街並み、微かに小鳥のさえずり",
-      "おしゃれなレストラン　小道具：おいしいごはん、おしゃれな雰囲気、ゆったりと流れる時間",
-    ]
-    table_5_6 = [
-      "何処ともしれない暗がり　小道具：薄暗がりの中、微かに見えるのは互いの表情くらい",
-      "寂れた喫茶店　小道具：姿を見せないマスター、その孫娘が持ってくる珈琲、静かなひととき",
-      "階段の下、秘密のお茶会　小道具：知る人ぞ知る階段下スペースのお茶会、今日はあなたたちだけ",
-      "学生寮の廊下　小道具：滅多に人とすれ違わない学生寮の廊下、窓の外には中庭が見える",
-      "ふたりの部屋　小道具：パートナーと共に暮らすあなたの部屋、内装や小物はお気に召すまま",
-      "願いの決闘場　小道具：決闘の場、ステラナイトたちの花章が咲き誇る場所",
-    ]
-
-    table = [table_1_2, table_1_2,
-             table_3_4, table_3_4,
-             table_5_6, table_5_6,].flatten
-
-    text, index = get_table_by_d66(table)
-
-    result = "#{tableName}(#{index}) ＞ #{text}"
-    return result
   end
 
   def getSchoolTableDiceCommandResult(command)
@@ -244,53 +168,6 @@ MESSAGETEXT
     return result
   end
 
-  def getTpicTableDiceCommandResult(command)
-    return unless command == "STC"
-
-    tableName = "シチュエーション表C：話題"
-
-    table_1_3 = [
-      "未来の話：決闘を勝ち抜いたら、あるいは負けてしまったら……未来のふたりはどうなるのだろう。",
-      "衣服の話：冴えない服を着たりしていないか？　あるいはハイセンス過ぎたりしないだろうか。よぉし、私が選んであげよう!!",
-      "ステラバトルの話：世界の未来は私たちにかかっている。頭では分かっていても、まだ感情が追いつかないな……。",
-      "おいしいごはんの話：おいしいごはんは正義。１００年前も６４０５年前も異世界だろうと、きっと変わらない真理なのだ。おかわり！",
-      "家族の話：生徒たちは寮生活が多い。離れて暮らす家族は、どんな人たちなのか。いつかご挨拶に行きたいと言い出したりしても良いだろう。",
-      "次の週末の話：週末、何をしますか？　願いをかけた決闘の合間、日常のひとときも、きっと大切な時間に違いない。",
-    ]
-
-    table_4_6 = [
-      "好きな人の話：……好きな人、いるんですか？　これはきっと真剣な話。他の何よりも重要な話だ。",
-      "子供の頃の話：ちいさな頃、パートナーはどんな子供だったのだろうか。どんな遊びをしたのだろうか。",
-      "好きなタイプの話：パートナーはどんな人が好みなのでしょうか……。気になります、えぇ。",
-      "思い出話：ふたりの思い出、あるいは出会う前の思い出の話。",
-      "願いの話：叶えたい願いがあるからこそ、ふたりは出会った。この戦いに勝利したら、どんな形で願いを叶えるのだろうか。",
-      "ねぇ、あの子誰？：この前見かけたパートナーと一緒にいた子。あの子誰？だーれー!?　むー!!"
-    ]
-
-    table = [table_1_3, table_1_3, table_1_3,
-             table_4_6, table_4_6, table_4_6, ].flatten
-
-    text, index = get_table_by_d66(table)
-
-    result = "#{tableName}(#{index}) ＞ #{text}"
-    return result
-  end
-
-  def getAllSituationTableDiceCommandResult(command)
-    return unless command == "ALLS"
-
-    commands = ['STA', 'STB', 'STC']
-
-    result = ""
-
-    commands.each_with_index do |cmd, i|
-      result += "\n" unless i == 0
-      result += analyzeDiceCommandResultMethod(cmd)
-    end
-
-    return result
-  end
-
   private
 
   D6 = 6
@@ -315,7 +192,11 @@ MESSAGETEXT
     output
   end
 
-  def roll_personality_table()
+  def roll_all_situation_tables
+    %w(STA STB STC).map { |command| TABLES[command].roll(bcdice) }.join("\n")
+  end
+
+  def roll_personality_table
     value1, index1 = get_table_by_d66(PERSONALITY_TABLE)
     value2, index2 = get_table_by_d66(PERSONALITY_TABLE)
     return "性格表(#{index1},#{index2}) ＞ #{value1}にして#{value2}"
@@ -332,6 +213,87 @@ MESSAGETEXT
 
     return "フラグメント表(#{indexes.join(',')}) ＞ #{values.join(',')}"
   end
+
+  THEME_TABLE = [
+    %w(未来 占い 遠雷 恋心 歯磨き 鏡).freeze,
+    %w(過去 キス ささやき声 黒い感情 だっこ 青空).freeze,
+    %w(童話 決意 風の音 愛情 寝顔 鎖).freeze,
+    %w(ふたりの秘密 アクシデント！ 小鳥の鳴き声 笑顔 食事 宝石).freeze,
+    %w(思い出 うとうと 鼓動 嫉妬 ベッド 泥).freeze,
+    %w(恋の話 デート ため息 内緒話 お風呂 小さな傷).freeze,
+  ].freeze
+
+  SITUATION_TABLE_A = %w{
+    朝、誰もいない
+    騒がしい昼間の
+    寂しい夕暮れの横たわる
+    星の瞬く夜、
+    静謐の夜更けに包まれた
+    夜明け前の
+  }.freeze
+
+  SITUATION_TABLE_B_1_2 = [
+    "教室 　小道具：窓、机、筆記用具、チョークと黒板、窓の外から聞こえる部活動の声",
+    "カフェテラス　小道具：珈琲、紅茶、お砂糖とミルク、こちらに手を振っている学友",
+    "学園の中庭　小道具：花壇、鳥籠めいたエクステリア、微かに聴こえる鳥の囁き",
+    "音楽室　小道具：楽器、楽譜、足踏みオルガン、壁に掛けられた音楽家の肖像画",
+    "図書館　小道具：高い天井、天井に迫る程の本棚、無数に収められた本",
+    "渡り廊下　小道具：空に届きそうな高さ、遠くに別の学園が見える、隣を飛び過ぎて行く鳥",
+  ].freeze
+
+  SITUATION_TABLE_B_3_4 = [
+    "花の咲き誇る温室　小道具：むせ返るような花の香り、咲き誇る花々、ガラス越しの陽光",
+    "アンティークショップ　小道具：アクセサリーから置物まで、見慣れない古い機械は地球時代のもの？",
+    "ショッピングモール　小道具：西欧の街並みを思わせるショッピングモール、衣類に食事、お茶屋さんも",
+    "モノレール　小道具：車窓から覗くアーセルトレイの街並み、乗客はあなたたちだけ",
+    "遊歩道　小道具：等間隔に並ぶ街路樹、レンガ造りの街並み、微かに小鳥のさえずり",
+    "おしゃれなレストラン　小道具：おいしいごはん、おしゃれな雰囲気、ゆったりと流れる時間",
+  ].freeze
+
+  SITUATION_TABLE_B_5_6 = [
+    "何処ともしれない暗がり　小道具：薄暗がりの中、微かに見えるのは互いの表情くらい",
+    "寂れた喫茶店　小道具：姿を見せないマスター、その孫娘が持ってくる珈琲、静かなひととき",
+    "階段の下、秘密のお茶会　小道具：知る人ぞ知る階段下スペースのお茶会、今日はあなたたちだけ",
+    "学生寮の廊下　小道具：滅多に人とすれ違わない学生寮の廊下、窓の外には中庭が見える",
+    "ふたりの部屋　小道具：パートナーと共に暮らすあなたの部屋、内装や小物はお気に召すまま",
+    "願いの決闘場　小道具：決闘の場、ステラナイトたちの花章が咲き誇る場所",
+  ].freeze
+
+  SITUATION_TABLE_B = [
+    SITUATION_TABLE_B_1_2,
+    SITUATION_TABLE_B_1_2,
+    SITUATION_TABLE_B_3_4,
+    SITUATION_TABLE_B_3_4,
+    SITUATION_TABLE_B_5_6,
+    SITUATION_TABLE_B_5_6,
+  ].freeze
+
+  SITUATION_TABLE_C_1_2_3 = [
+    "未来の話：決闘を勝ち抜いたら、あるいは負けてしまったら……未来のふたりはどうなるのだろう。",
+    "衣服の話：冴えない服を着たりしていないか？　あるいはハイセンス過ぎたりしないだろうか。よぉし、私が選んであげよう!!",
+    "ステラバトルの話：世界の未来は私たちにかかっている。頭では分かっていても、まだ感情が追いつかないな……。",
+    "おいしいごはんの話：おいしいごはんは正義。１００年前も６４０５年前も異世界だろうと、きっと変わらない真理なのだ。おかわり！",
+    "家族の話：生徒たちは寮生活が多い。離れて暮らす家族は、どんな人たちなのか。いつかご挨拶に行きたいと言い出したりしても良いだろう。",
+    "次の週末の話：週末、何をしますか？　願いをかけた決闘の合間、日常のひとときも、きっと大切な時間に違いない。",
+  ].freeze
+
+  SITUATION_TABLE_C_4_5_6 = [
+    "好きな人の話：……好きな人、いるんですか？　これはきっと真剣な話。他の何よりも重要な話だ。",
+    "子供の頃の話：ちいさな頃、パートナーはどんな子供だったのだろうか。どんな遊びをしたのだろうか。",
+    "好きなタイプの話：パートナーはどんな人が好みなのでしょうか……。気になります、えぇ。",
+    "思い出話：ふたりの思い出、あるいは出会う前の思い出の話。",
+    "願いの話：叶えたい願いがあるからこそ、ふたりは出会った。この戦いに勝利したら、どんな形で願いを叶えるのだろうか。",
+    "ねぇ、あの子誰？：この前見かけたパートナーと一緒にいた子。あの子誰？だーれー!?　むー!!"
+  ].freeze
+
+  SITUATION_TABLE_C = [
+    SITUATION_TABLE_C_1_2_3,
+    SITUATION_TABLE_C_1_2_3,
+    SITUATION_TABLE_C_1_2_3,
+    SITUATION_TABLE_C_4_5_6,
+    SITUATION_TABLE_C_4_5_6,
+    SITUATION_TABLE_C_4_5_6,
+  ].freeze
 
   GAKUEN_TABLE = [
     "アーセルトレイ公立大学",
@@ -829,6 +791,23 @@ MESSAGETEXT
   ].freeze
 
   TABLES = {
+    "TT" => D66GridTable.new(
+      "お題表",
+      THEME_TABLE
+    ),
+    "STA" => Table.new(
+      "シチュエーション表A：時間",
+      "1D6",
+      SITUATION_TABLE_A
+    ),
+    "STB" => D66GridTable.new(
+      "シチュエーション表B：場所",
+      SITUATION_TABLE_B
+    ),
+    "STC" => D66GridTable.new(
+      "シチュエーション表C：話題",
+      SITUATION_TABLE_C
+    ),
     "GAT" => Table.new(
       "所属組織決定",
       "1D6",
@@ -900,5 +879,5 @@ MESSAGETEXT
     ),
   }.freeze
 
-  setPrefixes(['\d+SK\d?(,\d>\d)*', 'TT', 'STA', 'STB', 'STB2', 'STC', 'ALLS', 'PET', 'FT\d*'] + TABLES.keys)
+  setPrefixes(['\d+SK\d?(,\d>\d)*', 'STB2', 'ALLS', 'PET', 'FT\d*'] + TABLES.keys)
 end
