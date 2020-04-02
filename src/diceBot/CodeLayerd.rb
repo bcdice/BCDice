655ea9473de814f5518f9d51afadc7061830a3d9
GenKuzumochi <genkuzumochi@gmail.com>
2020-04-02 20:07:04 +0900

    コード：レイヤードのクリティカル値の変更に対応

--- src/diceBot/CodeLayerd.rb
+++ src/diceBot/CodeLayerd.rb
@@ -16,15 +16,19 @@ class CodeLayerd < DiceBot
 
   # ダイスボットの使い方
   HELP_MESSAGE = <<MESSAGETEXT
-・行為判定（nCL@m+x または nCL+x@m） クリティカル・ファンブル判定あり
-  n個のD10でmを判定値とした行為判定を行う。mは省略可能。（@6扱い）
-  修正値x(固定成功度)もつけられます。
-  例）7CL>=5 ：サイコロ7個で判定値6のロールを行い、目標値5に対して判定
-  例）4CL@7  ：サイコロ4個で判定値7のロールを行い達成値を出す
-  例）4CL+2@7 または 4CL@7+2  ：サイコロ4個で判定値7のロールを行い達成値を出し、修正値2を足す。
+・行為判定（nCL@m[c]+x または nCL+x@m[c]） クリティカル・ファンブル判定あり
+  (ダイス数)CL+(修正値)@(判定値)[(クリティカル値)]+(修正値2)
+
+  @m,[c],+xは省略可能。(@6[1]として処理)
+  n個のD10でmを判定値、cをクリティカル値とした行為判定を行う。
+  例）
+  7CL>=5 ：サイコロ7個で判定値6のロールを行い、目標値5に対して判定
+  4CL@7  ：サイコロ4個で判定値7のロールを行い達成値を出す
+  4CL+2@7 または 4CL@7+2  ：サイコロ4個で判定値7のロールを行い達成値を出し、修正値2を足す。
+  4CL[2] ：サイコロ4個でクリティカル値2のロールを行う。
 MESSAGETEXT
 
-  setPrefixes(['\d*CL[@\d]*.*'])
+  setPrefixes(['\d*CL([+-]\d+)?[@\d]*.*'])
 
   def isGetOriginalMessage
     true
@@ -36,14 +40,15 @@ MESSAGETEXT
     result = ''
 
     case command
-    when /(\d+)?CL([+-]\d+)?(\@?(\d))?([+-]\d+)?(>=(\d+))?/i
+    when /(\d+)?CL([+-]\d+)?(\@(\d))?(\[(\d{1,2})\])?([+-]\d+)?(>=(\d+))?/i
       m = Regexp.last_match
       base = (m[1] || 1).to_i
       modifier1 = (m[2].to_i || 0)
       target = (m[4] || 6).to_i
-      modifier2 = (m[5].to_i || 0)
-      diff = m[7].to_i
-      result = checkRoll(base, target, diff, modifier1 + modifier2)
+      criticalTarget = (m[6] || 1).to_i
+      modifier2 = (m[7].to_i || 0)
+      diff = m[9].to_i
+      result = checkRoll(base, target, criticalTarget, diff, modifier1 + modifier2)
     end
 
     return nil if result.empty?
@@ -51,11 +56,12 @@ MESSAGETEXT
     return "#{command} ＞ #{result}"
   end
 
-  def checkRoll(base, target, diff, modifier)
+  def checkRoll(base, target, criticalTarget, diff, modifier)
     result = ""
 
     base = getValue(base)
     target = getValue(target)
+    criticalTarget = getValue(criticalTarget)
 
     return result if base < 1
 
@@ -68,18 +74,20 @@ MESSAGETEXT
     diceList = diceText.split(/,/).collect { |i| i.to_i }.sort
 
     result += " ＞ [#{diceList.join(',')}]#{format_modifier(modifier)} ＞ "
-    result += getRollResultString(diceList, target, diff, modifier)
+    result += getRollResultString(diceList, target, criticalTarget, diff, modifier)
 
     return result
   end
 
-  def getRollResultString(diceList, target, diff, modifier)
-    successCount, criticalCount = getSuccessInfo(diceList, target)
+  def getRollResultString(diceList, target, criticalTarget, diff, modifier)
+    successCount, criticalCount = getSuccessInfo(diceList, target, criticalTarget)
 
     successTotal = successCount + criticalCount + modifier
     result = ""
 
-    result += "判定値[#{target}] 達成値[#{successCount}]"
+    result += "判定値[#{target}] "
+    result += "クリティカル値[#{criticalTarget}] " unless criticalTarget == 1
+    result += "達成値[#{successCount}]"
     result += "+クリティカル[#{criticalCount}]" if criticalCount > 0
     result += format_modifier(modifier)
     result += "=[#{successTotal}]" if criticalCount > 0 || modifier != 0
@@ -98,15 +106,15 @@ MESSAGETEXT
     return "失敗"
   end
 
-  def getSuccessInfo(diceList, target)
-    debug("checkSuccess diceList, target", diceList, target)
+  def getSuccessInfo(diceList, target, criticalTarget)
+    debug("checkSuccess diceList, target, criticalTarget", diceList, target, criticalTarget)
 
     successCount  = 0
     criticalCount = 0
 
     diceList.each do |dice|
       successCount += 1 if dice <= target
-      criticalCount += 1 if dice == 1
+      criticalCount += 1 if dice <= criticalTarget
     end
 
     return successCount, criticalCount
