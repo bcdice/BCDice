04a74f2b492981acdc4a4ebcf4cc9b5e532e5f47
FredGreenfield <fgfbyt@gmail.com>
2020-05-15 18:28:45 +0900

    コマンドの見直し(FSSx→FSx,FSWxBx→WxBx) WxBxコマンド時のダイス表記を削除し、白の合計、黒の合計を表記するように修正

--- src/diceBot/Fiasco.rb
+++ src/diceBot/Fiasco.rb
@@ -13,10 +13,10 @@ class Fiasco < DiceBot
 
   # ダイスボットの使い方
   HELP_MESSAGE = <<INFO_MESSAGE_TEXT
-  ・判定コマンド(FSSx, FSWxBx)
-    相関図・転落要素用(FSSx)：相関図や転落要素のためにx個ダイスを振り、出目ごとに分類する
-    黒白差分判定用(FSWxBx)  ：転落、残響のために白ダイス(W指定)と黒ダイス(B指定)で差分を求める
-      ※ WとBは省略(FSBx, FSWx)、入替(FSWxBx,FSBxWx)可能
+  ・判定コマンド(FSx, WxBx)
+    相関図・転落要素用(FSx)：相関図や転落要素のためにx個ダイスを振り、出目ごとに分類する
+    黒白差分判定用(WxBx)  ：転落、残響のために白ダイス(W指定)と黒ダイス(B指定)で差分を求める
+      ※ WとBは片方指定(Bx, Wx)、入替指定(WxBx,BxWx)可能
 
 INFO_MESSAGE_TEXT
 
@@ -28,17 +28,14 @@ INFO_MESSAGE_TEXT
   BW_SECOND_DICE_INDEX = 5
   BW_SECOND_DICE_TAG_INDEX = 4
 
-  BW_SECOND_DICE_WHITE_TAG = "W"
-  BW_SECOND_DICE_BLACK_TAG = "B"
+  START_COMMAND_TAG = "FS"
+  W_DICEROLL_COMMAND_TAG = "W"
+  B_DICEROLL_COMMAND_TAG = "B"
 
-  START_COMMAND_TAG = "FSS"
-  WB_DICEROLL_COMMAND_TAG = "FSW"
-  BW_DICEROLL_COMMAND_TAG = "FSB"
-
-  setPrefixes(['FS.*'])
+  setPrefixes(['(FS|[WB])(\d+).*'])
 
   def rollDiceCommand(command)
-    m = /\A(FS[SWB]?)(\d+)(([WB])(\d+))?/.match(command)
+    m = /\A(FS|[WB])(\d+)(([WB])(\d+))?/.match(command)
     unless m
       return ''
     end
@@ -65,27 +62,26 @@ INFO_MESSAGE_TEXT
   end
 
   def makeWhiteBlackDiceRoll(type, m)
-    if type == WB_DICEROLL_COMMAND_TAG
+    if type == W_DICEROLL_COMMAND_TAG
       whiteTotal, whiteDiceText, blackTotal, blackDiceText = makeArgsDiceRoll(m[BW_FIRST_DICE_INDEX], m[BW_SECOND_DICE_INDEX])
-      result = "白ダイス[#{whiteDiceText}]"
+      result = "白#{whiteTotal}[#{whiteDiceText}]"
       if blackDiceText
-        if m[BW_SECOND_DICE_TAG_INDEX].start_with?(BW_SECOND_DICE_WHITE_TAG)
-          return "#{m}：白ダイス指定(#{BW_SECOND_DICE_WHITE_TAG})は重複できません。"
+        if m[BW_SECOND_DICE_TAG_INDEX] == W_DICEROLL_COMMAND_TAG
+          return "#{m}：白指定(#{W_DICEROLL_COMMAND_TAG})は重複できません。"
         end
 
-        result += " 黒ダイス[#{blackDiceText}]"
+        result += " 黒#{blackTotal}[#{blackDiceText}]"
       end
-    elsif type == BW_DICEROLL_COMMAND_TAG
+    elsif type == B_DICEROLL_COMMAND_TAG
       blackTotal, blackDiceText, whiteTotal, whiteDiceText = makeArgsDiceRoll(m[BW_FIRST_DICE_INDEX], m[BW_SECOND_DICE_INDEX])
-      result = "黒ダイス[#{blackDiceText}]"
+      result = "黒#{blackTotal}[#{blackDiceText}]"
       if whiteDiceText
-        if m[BW_SECOND_DICE_TAG_INDEX].start_with?(BW_SECOND_DICE_BLACK_TAG)
-          return "#{m}：黒ダイス指定(#{BW_SECOND_DICE_BLACK_TAG})は重複できません。"
+        if m[BW_SECOND_DICE_TAG_INDEX] == B_DICEROLL_COMMAND_TAG
+          return "#{m}：黒指定(#{B_DICEROLL_COMMAND_TAG})は重複できません。"
         end
 
-        result += " 白ダイス[#{whiteDiceText}]"
+        result += " 白#{whiteTotal}[#{whiteDiceText}]"
       end
-
     else
       return ''
     end
