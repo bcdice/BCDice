6b364e2de704f8dfbf51214452b41b396e40be0f
SAKATA Sinji <ysakasin@gmail.com>
2020-05-16 16:49:08 +0900

    Release Ver2.06.00

--- src/bcdiceCore.rb
+++ src/bcdiceCore.rb
@@ -88,7 +88,7 @@ class BCDice
   # 設定コマンドのパターン
   SET_COMMAND_PATTERN = /\Aset\s+(.+)/i.freeze
 
-  VERSION = "2.05.00".freeze
+  VERSION = "2.06.00".freeze
 
   attr_reader :cardTrader
   attr_reader :rand_results, :detailed_rand_results
