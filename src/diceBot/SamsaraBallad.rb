0f96992c1a6931546266a18177da26f4d7742401
Hideyo Mikisato <h.mikisato@gmail.com>
2020-05-21 20:33:48 +0900

    C値とF値のコマンド順序入れ替え、スワップロール時に1d10を2回使うように変更

--- src/diceBot/SamsaraBallad.rb
+++ src/diceBot/SamsaraBallad.rb
@@ -28,12 +28,12 @@ SBS@7#3<=80 習熟を得た技能で、C値7、F値3で成功率80%の判定
 MESSAGETEXT
 
   # ダイスボットで使用するコマンドを配列で列挙する
-  setPrefixes(['SBS?(@\d#\d)?.*'])
+  setPrefixes(['SBS?(#\d@\d)?.*'])
 
   def initialize
     @swap = false
-    @c_value = 10
     @f_value = -1
+    @c_value = 10
     super
   end
 
@@ -41,29 +41,29 @@ MESSAGETEXT
     debug("rollDiceCommand Begin")
 
     case command
-    when /SBS@(\d)#(\d)/i
+    when /SBS#(\d)@(\d)/i
       debug("Swap roll with F and C")
       @swap = true
-      @c_value = Regexp.last_match(1).to_i
-      @f_value = Regexp.last_match(2).to_i
+      @f_value = Regexp.last_match(1).to_i
+      @c_value = Regexp.last_match(2).to_i
       return getCheckResult(command)
-    when /SB@(\d)#(\d)/i
+    when /SB#(\d)@(\d)/i
       debug("Normal roll with F and C")
       @swap = false
-      @c_value = Regexp.last_match(1).to_i
-      @f_value = Regexp.last_match(2).to_i
+      @f_value = Regexp.last_match(1).to_i
+      @c_value = Regexp.last_match(2).to_i
       return getCheckResult(command)
     when /SBS/i
       debug("Swap roll")
       @swap = true
-      @c_value = 10
       @f_value = -1
+      @c_value = 10
       return getCheckResult(command)
     when /SB/i
       debug("Normal roll")
       @swap = false
-      @c_value = 10
       @f_value = -1
+      @c_value = 10
       return getCheckResult(command)
     end
     return nil
@@ -72,27 +72,35 @@ MESSAGETEXT
   def getCheckResult(command)
     diff = 0
 
-    if (m = %r{SBS?(@\d#\d)?<=([+-/*\d]+)}i.match(command))
+    if (m = %r{SBS?(#\d@\d)?<=([+-/*\d]+)}i.match(command))
       diff = ArithmeticEvaluator.new.eval(m[2])
     end
 
-    total_n, = roll(1, 100)
-    dice_label = total_n.to_s
-    if total_n < 100 && @swap
-      x = total_n.div(10)
-      y = total_n % 10
-      if x > y
-        new_total_n = 10 * y + x
-        dice_label = "#{total_n} -> #{new_total_n}"
-        total_n = new_total_n
+    if @swap
+      d100_n1, = roll(1, 10)
+      d100_n2, = roll(1, 10)
+      d100_dice = [d100_n1, d100_n2]
+      min_n = d100_dice.min
+      max_n = d100_dice.max
+      debug(min_n, max_n)
+      if min_n == 10
+        total_n = 100
+      elsif max_n == 10
+        total_n = min_n
+      else
+        total_n = 10 * min_n + max_n
       end
+      dice_label = "#{d100_n1},#{d100_n2} ＞ #{total_n}"
+    else
+      total_n, = roll(1, 100)
+      dice_label = total_n.to_s
     end
 
     is_fumble = @f_value >= 0 && total_n % 10 <= @f_value
     is_critical = !is_fumble && @c_value < 10 && total_n % 10 >= @c_value
 
     if diff > 0
-      output = "(1D100<=#{diff})"
+      output = "(D100<=#{diff})"
       output += " ＞ #{dice_label}"
       if (total_n <= diff) && (total_n < 100)
         if is_fumble
@@ -106,7 +114,7 @@ MESSAGETEXT
         output += " ＞ 失敗"
       end
     else
-      output = "1D100 ＞ #{dice_label}"
+      output = "D100 ＞ #{dice_label}"
       if is_fumble
         output += " ＞ ファンブル"
       elsif is_critical
