b0f6a8b75e68bc2ccc18b51b24aa9c029fa7a46e
FredGreenfield <fgfbyt@gmail.com>
2020-06-22 21:41:23 +0900

    難易度を設定可能にし判定結果を出すように修正 ダイスボット名を英語に修正

--- src/diceBot/YearZeroEngine.rb
+++ src/diceBot/YearZeroEngine.rb
@@ -6,7 +6,7 @@ class YearZeroEngine < DiceBot
   ID = 'YearZeroEngine'
 
   # ゲームシステム名
-  NAME = 'イヤーゼロエンジン'
+  NAME = 'YearZeroEngine'
 
   # ゲームシステム名の読みがな
   SORT_KEY = 'いやあせろえんしん'
@@ -14,32 +14,41 @@ class YearZeroEngine < DiceBot
   # ダイスボットの使い方
   HELP_MESSAGE = <<INFO_MESSAGE_TEXT
 ・判定コマンド(YZEx+x+x)
-  YZE(能力ダイス数)+(技能ダイス数)+(修正ダイス数)  # YearZeroEngine(TALES FROM THE LOOP等)の判定(6を数える)
-  ※ 技能と修正ダイス数は省略可能
+  (難易度)YZE(能力ダイス数)+(技能ダイス数)+(修正ダイス数)  # YearZeroEngine(TALES FROM THE LOOP等)の判定(6を数える)
+  ※ 難易度と技能、修正ダイス数は省略可能
 INFO_MESSAGE_TEXT
 
-  ABILITY_INDEX    = 2 # 能力値ダイスのインデックス
-  SKILL_INDEX      = 4 # 技能値ダイスのインデックス
-  MODIFIED_INDEX   = 6 # 修正ダイスのインデックス
+  DIFFICULTY_INDEX   = 1 # 難易度のインデックス
+  COMMAND_TYPE_INDEX = 2 # コマンドタイプのインデックス
+  ABILITY_INDEX      = 3 # 能力値ダイスのインデックス
+  SKILL_INDEX        = 5 # 技能値ダイスのインデックス
+  MODIFIED_INDEX     = 7 # 修正ダイスのインデックス
 
-  setPrefixes(['YZE.*'])
+  setPrefixes(['(\d+)?YZE.*'])
 
   def rollDiceCommand(command)
-    m = /\A(YZE)(\d+)(\+(\d+))?(\+(\d+))?/.match(command)
+    m = /\A(\d+)?(YZE)(\d+)(\+(\d+))?(\+(\d+))?/.match(command)
     unless m
       return ''
     end
 
-    success_dice = 0
+    difficulty = m[DIFFICULTY_INDEX].to_i
+
+    command_type = m[COMMAND_TYPE_INDEX]
+
+    total_success_dice = 0
+
     match_text = m[ABILITY_INDEX]
-    ability_dice_text, success_dice = make_dice_roll(match_text, success_dice)
+    ability_dice_text, success_dice = make_dice_roll(match_text)
+    total_success_dice += success_dice
 
     dice_count_text = "(#{match_text}D6)"
     dice_text = ability_dice_text
 
     match_text = m[SKILL_INDEX]
     if match_text
-      skill_dice_text, success_dice = make_dice_roll(match_text, success_dice)
+      skill_dice_text, success_dice = make_dice_roll(match_text)
+      total_success_dice += success_dice
 
       dice_count_text += "+(#{match_text}D6)"
       dice_text += "+#{skill_dice_text}"
@@ -47,24 +56,39 @@ INFO_MESSAGE_TEXT
 
     match_text = m[MODIFIED_INDEX]
     if match_text
-      modified_dice_text, success_dice = make_dice_roll(match_text, success_dice)
+      modified_dice_text, success_dice = make_dice_roll(match_text)
+      total_success_dice += success_dice
 
       dice_count_text += "+(#{match_text}D6)"
       dice_text += "+#{modified_dice_text}"
     end
 
-    return "#{dice_count_text} ＞ #{dice_text} 成功数:#{success_dice}"
+    result_text = "#{dice_count_text} ＞ #{dice_text} 成功数:#{total_success_dice}"
+    if difficulty > 0
+      if total_success_dice >= difficulty
+        result_text = "#{result_text} 難易度=#{difficulty}:判定成功！"
+      else
+        result_text = "#{result_text} 難易度=#{difficulty}:判定失敗！"
+      end
+    end
+
+    return result_text
   end
 
-  def make_dice_roll(match_text, success_dice)
+  def make_dice_roll(match_text)
+    botch_dice = 0
+    success_dice = 0
+
     dice = match_text.to_i
     _, dice_text, = roll(dice, 6)
 
     dice_text.split(',').each do |take_dice|
       if take_dice == "6"
         success_dice += 1
+      elsif take_dice == "1"
+        botch_dice += 1
       end
     end
-    return "[#{dice_text}]", success_dice
+    return "[#{dice_text}]", success_dice, botch_dice
   end
 end
