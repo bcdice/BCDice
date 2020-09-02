08fa0462b63c7ebdbde1b597e2a4c1bdd66fa1f7
SAKATA Sinji <ysakasin@gmail.com>
2020-09-02 18:56:26 +0900

    フォーマット系のモジュールをBCDice配下に

--- lib/bcdice/normalize.rb
+++ lib/bcdice/normalize.rb
@@ -1,36 +1,38 @@
-module Normalize
-  module_function
+module BCDice
+  module Normalize
+    module_function
 
-  # 比較演算子をシンボルに正規化する
-  #
-  # @param op [String]
-  # @return [Symbol, nil]
-  def comparison_operator(op)
-    case op
-    when /<=|=</
-      :<=
-    when />=|=>/
-      :>=
-    when /<>|!=|=!/
-      :'!='
-    when /</
-      :<
-    when />/
-      :>
-    when /\=/
-      :==
+    # 比較演算子をシンボルに正規化する
+    #
+    # @param op [String]
+    # @return [Symbol, nil]
+    def comparison_operator(op)
+      case op
+      when /<=|=</
+        :<=
+      when />=|=>/
+        :>=
+      when /<>|!=|=!/
+        :'!='
+      when /</
+        :<
+      when />/
+        :>
+      when /\=/
+        :==
+      end
     end
-  end
 
-  # 目標値を正規化する
-  #
-  # @param val [String]
-  # @return [Integer, String] 整数か'?'
-  def target_number(val)
-    if val == '?'
-      val
-    else
-      val.to_i
+    # 目標値を正規化する
+    #
+    # @param val [String]
+    # @return [Integer, String] 整数か'?'
+    def target_number(val)
+      if val == '?'
+        val
+      else
+        val.to_i
+      end
     end
   end
 end
