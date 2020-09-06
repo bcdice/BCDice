07a13df0df83bb2deb50e10b9647de676f262c12
SAKATA Sinji <ysakasin@gmail.com>
2020-09-06 22:04:32 +0900

    MeikyuKingdomを複数ファイルに分割する

---
+++ lib/bcdice/game_system/meikyu_kingdom/item_table.rb
@@ -0,0 +1,383 @@
+module BCDice
+  module GameSystem
+    class MeikyuKingdom
+      # デバイスファクトリー(1D6)
+      def mk_device_factory_table()
+        output = mk_item_decide_table(@randomizer.roll_once(6))
+
+        dice = @randomizer.roll_sum(2, 6)
+        output = output + " / " + mk_item_features_table(dice)
+        return output
+      end
+
+      # アイテムカテゴリ決定表(1D6)
+      def mk_item_decide_table(num)
+        functionTable = [
+          [1, lambda { mk_weapon_item_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [2, lambda { mk_life_item_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [3, lambda { mk_rest_item_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [4, lambda { mk_search_item_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [5, lambda { mk_rare_weapon_item_table(@randomizer.roll_d66(D66SortType::NO_SORT)) }],
+          [6, lambda { mk_rare_item_table(@randomizer.roll_d66(D66SortType::NO_SORT)) }],
+        ]
+        return get_table_by_number(num, functionTable)
+      end
+
+      # 武具アイテム表(D66)
+      def mk_weapon_item_table(num)
+        table = [
+          [11, "だんびら"],
+          [12, "だんびら"],
+          [13, "ダガー"],
+          [14, "戦斧"],
+          [15, "盾"],
+          [16, "鑓"],
+          [22, "籠手（だんびら）"],
+          [23, "手裏剣"],
+          [24, "石弓"],
+          [25, "甲冑"],
+          [26, "戦鎚"],
+          [33, "大弓（だんびら）"],
+          [34, "爆弾"],
+          [35, "鉄砲"],
+          [36, "大剣"],
+          [44, "拳銃（だんびら）"],
+          [45, "ホウキ"],
+          [46, "徹甲弾"],
+          [55, "だんびら"],
+          [56, "大砲"],
+          [66, "だんびら"],
+        ]
+
+        return get_table_by_number(num, table)
+      end
+
+      # 生活アイテム表(D66)
+      def mk_life_item_table(num)
+        table = [
+          [11, "バックパック"],
+          [12, "バックパック"],
+          [13, "鍋"],
+          [14, "クラッカー"],
+          [15, "がまぐち"],
+          [16, "マント"],
+          [22, "法衣（バックパック）"],
+          [23, "カード"],
+          [24, "エプロン"],
+          [25, "住民台帳"],
+          [26, "携帯電話"],
+          [33, "召喚鍵（バックパック）"],
+          [34, "肖像画"],
+          [35, "衣装"],
+          [36, "山吹色のお菓子"],
+          [44, "バックパック"],
+          [45, "眼鏡"],
+          [46, "クレジットカード"],
+          [55, "バックパック"],
+          [56, "魔道書"],
+          [66, "バックパック"],
+        ]
+
+        return get_table_by_number(num, table)
+      end
+
+      # 回復アイテム表(D66)
+      def mk_rest_item_table(num)
+        table = [
+          [11, "お弁当"],
+          [12, "お弁当"],
+          [13, "特効薬"],
+          [14, "保存食"],
+          [15, "担架"],
+          [16, "珈琲"],
+          [22, "軟膏（お弁当）"],
+          [23, "チョコレート"],
+          [24, "お酒"],
+          [25, "フルコース"],
+          [26, "ポーション"],
+          [33, "お弁当"],
+          [34, "救急箱"],
+          [35, "強壮剤"],
+          [36, "迷宮保険"],
+          [44, "お弁当"],
+          [45, "科学調味料"],
+          [46, "惚れ薬"],
+          [55, "お弁当"],
+          [56, "復活薬"],
+          [66, "お弁当"],
+        ]
+
+        return get_table_by_number(num, table)
+      end
+
+      # 探索アイテム表(D66)
+      def mk_search_item_table(num)
+        table = [
+          [11, "星の欠片"],
+          [12, "星の欠片"],
+          [13, "旗"],
+          [14, "お守り"],
+          [15, "拷問具"],
+          [16, "パワーリスト"],
+          [22, "工具（星の欠片）"],
+          [23, "テント"],
+          [24, "楽器"],
+          [25, "使い魔"],
+          [26, "乗騎"],
+          [33, "迷宮迷彩（星の欠片）"],
+          [34, "罠百科"],
+          [35, "迷宮防護服"],
+          [36, "地図"],
+          [44, "星の欠片"],
+          [45, "時計"],
+          [46, "もぐら棒"],
+          [55, "星の欠片"],
+          [56, "カボチャの馬車"],
+          [66, "星の欠片"],
+        ]
+
+        return get_table_by_number(num, table)
+      end
+
+      # レア武具アイテム表(1D6+1D6)
+      def mk_rare_weapon_item_table(num)
+        table = [
+          [11, "虚弾"],
+          [12, "怪物毒"],
+          [13, "小鬼の襟巻"],
+          [14, "喇叭銃"],
+          [15, "蛍矢"],
+          [16, "大盾"],
+          [21, "まわし"],
+          [22, "怪物毒"],
+          [23, "しゃべる剣"],
+          [24, "小麦粉"],
+          [25, "王笏"],
+          [26, "服従の鞭"],
+          [31, "ぬいぐるみ"],
+          [32, "魔杖"],
+          [33, "怪物毒"],
+          [34, "星衣"],
+          [35, "聖印"],
+          [36, "獣の毛皮"],
+          [41, "日傘"],
+          [42, "チェインソード"],
+          [43, "邪眼"],
+          [44, "怪物毒"],
+          [45, "徒手空拳"],
+          [46, "バカには見えない鎧"],
+          [51, "ビキニアーマー"],
+          [52, "輝く者"],
+          [53, "貪る者"],
+          [54, "滅ぼす者"],
+          [55, "機械の体"],
+          [56, "破城槌"],
+          [61, "刈り取る者"],
+          [62, "貫く者"],
+          [63, "黄金の鶴嘴"],
+          [64, "ムラサマ"],
+          [65, "蒸気甲冑"],
+          [66, "王剣"],
+        ]
+
+        return get_table_by_number(num, table)
+      end
+
+      # レア一般アイテム表(1D6+1D6)
+      def mk_rare_item_table(num)
+        table = [
+          [11, "ブルーリボン"],
+          [12, "聖痕"],
+          [13, "剥製"],
+          [14, "愚者の冠"],
+          [15, "名刺"],
+          [16, "種籾"],
+          [21, "香水"],
+          [22, "守りの指輪（名刺）"],
+          [23, "煙玉"],
+          [24, "悪名"],
+          [25, "藁人形"],
+          [26, "パワー餌"],
+          [31, "王妃の鏡"],
+          [32, "蓄音機"],
+          [33, "無限の心臓（名刺）"],
+          [34, "星籠"],
+          [35, "水晶球"],
+          [36, "転ばぬ先の杖"],
+          [41, "悟りの書"],
+          [42, "操りロープ"],
+          [43, "盗賊の七つ道具"],
+          [44, "携帯算術機（名刺）"],
+          [45, "棺桶"],
+          [46, "カメラ"],
+          [51, "不思議なたまご"],
+          [52, "ブーケ"],
+          [53, "露眼鏡"],
+          [54, "災厄王の遺物"],
+          [55, "経験値"],
+          [56, "鞍"],
+          [61, "視肉"],
+          [62, "玉璽"],
+          [63, "衛星帯"],
+          [64, "軍配"],
+          [65, "聖杯"],
+          [66, "愛"],
+        ]
+
+        return get_table_by_number(num, table)
+      end
+
+      # アイテムの特性決定表(2D6)
+      def mk_item_features_table(num)
+        output = ""
+        dice = @randomizer.roll_sum(2, 6)
+
+        if num <= 2
+          output = "「" + mk_item_power_table(@randomizer.roll_once(6)) + "」の神力を宿す"
+        elsif num <= 3
+          output = "寿命を持つ。寿命の値を決定する。" + "\n"
+          output += "さらに、" + mk_item_features_table(dice)
+        elsif num <= 4
+          output = "境界障壁を持つ。《HP》の値を決定する。"
+        elsif num <= 5
+          output = "銘を持つ。銘を決定する。"
+        elsif num <= 6
+          output = "合成具である。もう1つの機能は「" + mk_item_decide_table(@randomizer.roll_once(6)) + "」である。"
+        elsif num <= 7
+          output = "そのアイテムにレベルがあれば、レベルを1点上昇する。" + "\n"
+          output += "レベルが設定されていなければ、" + mk_item_features_table(dice)
+        elsif num <= 8
+          output = "「" + mk_item_jyumon_table(dice) + "」の呪紋を持つ。"
+        elsif num <= 9
+          output = "「" + mk_item_jyuka_table(@randomizer.roll_once(6)) + "」の呪禍を持つ。" + "\n"
+          output += "さらに、" + mk_item_features_table(dice)
+        elsif num <= 10
+          output = "高価だ。価格を設定する。"
+        elsif num <= 11
+          output = "「条件：" + mk_item_aptitude_table(@randomizer.roll_once(6)) + "」の適性を持つ。" + "\n"
+          output += "さらに、" + mk_item_features_table(dice)
+        else
+          output = "「" + mk_item_attribute_table(@randomizer.roll_once(6)) + "」の属性を持つ。"
+        end
+
+        return "特性[" + num.to_s + "]：" + output
+      end
+
+      # 神力決定表(1D6)
+      def mk_item_power_table(num)
+        table = [
+          [1, "〔才覚〕"],
+          [2, "〔魅力〕"],
+          [3, "〔探索〕"],
+          [4, "〔武勇〕"],
+          [5, "〈器〉"],
+          [6, "〈回避値〉"],
+        ]
+
+        return "[#{num}]" + get_table_by_number(num, table)
+      end
+
+      # 呪紋決定表(2D6)
+      def mk_item_jyumon_table(num)
+        table = [
+          [2, "モンスタースキル"],
+          [3, "便利スキル"],
+          [4, "芸能スキル"],
+          [5, "迷宮スキル"],
+          [6, "星術スキル"],
+          [7, "一般スキル"],
+          [8, "召喚スキル"],
+          [9, "科学スキル"],
+          [10, "交渉スキル"],
+          [11, "神官のクラススキル"],
+          [12, "ジョブスキル"],
+        ]
+
+        return "[#{num}]" + get_table_by_number(num, table)
+      end
+
+      # 呪禍表(1D6)
+      def mk_item_jyuka_table(num)
+        table = [
+          [1, "「呪い」のバッドステータス"],
+          [2, "「肥満」のバッドステータス"],
+          [3, "「愚か」のバッドステータス"],
+          [4, "サイクルの終了時に《HP》が1点減少する"],
+          [5, "条件を満たしても誰とも人間関係を結べない"],
+          [6, "〈器〉が1点減少する"],
+        ]
+
+        return "[#{num}]" + get_table_by_number(num, table)
+      end
+
+      # 適正表(1D6)
+      def mk_item_aptitude_table(num)
+        table = [
+          [1, "ランダムなクラス1種"],
+          [2, lambda { mk_family_business_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [3, lambda { mk_gender_table(@randomizer.roll_once(6)) + "性" }],
+          [4, "上級ジョブ"],
+          [5, "モンスタースキルを修得"],
+          [6, "童貞、もしくは処女"],
+        ]
+
+        return "[#{num}]" + get_table_by_number(num, table)
+      end
+
+      # 属性表(1D6)
+      def mk_item_attribute_table(num)
+        table = [
+          [1, "自然の力"],
+          [2, "幻夢の力"],
+          [3, "星炎の力"],
+          [4, "暗黒の力"],
+          [5, "聖なるの力"],
+          [6, "災厄の力"],
+        ]
+
+        return "[#{num}]" + get_table_by_number(num, table)
+      end
+
+      def mk_gender_table(num)
+        output = "1"
+
+        if num.odd?
+          output = "男"
+        else
+          output = "女"
+        end
+
+        return output
+      end
+
+      # 生まれ表(D66)
+      def mk_family_business_table(num)
+        table = [
+          [11, "星術師"],
+          [12, "魔道師"],
+          [13, "召喚師"],
+          [14, "博士"],
+          [15, "医者"],
+          [16, "貴族"],
+          [22, "宦官"],
+          [23, "武人"],
+          [24, "処刑人"],
+          [25, "衛視"],
+          [26, "商人"],
+          [33, "迷宮職人"],
+          [34, "亭主"],
+          [35, "料理人"],
+          [36, "寿ぎ屋"],
+          [44, "働きもの"],
+          [45, "狩人"],
+          [46, "冒険者"],
+          [55, "怠け者"],
+          [56, "盗賊"],
+          [66, "生まれ表の中から、好きなジョブ1つを選ぶ"],
+        ]
+
+        return "[#{num}]" + get_table_by_number(num, table)
+      end
+    end
+  end
+end
---
+++ lib/bcdice/game_system/meikyu_kingdom/placename_table.rb
@@ -0,0 +1,214 @@
+module BCDice
+  module GameSystem
+    class MeikyuKingdom
+      # 地名決定表
+      def mk_pn_decide_table(num)
+        output = ""
+
+        d1 = @randomizer.roll_once(6)
+        d2 = @randomizer.roll_once(6)
+        debug("d1", d1)
+        debug("d2", d2)
+
+        d1 = (d1 / 2.0).ceil.to_i
+        d2 = (d2 / 2.0).ceil.to_i
+
+        num.times do |_i|
+          output += "「" + mk_decoration_table(d1) + mk_placename_table(d2) + "」"
+        end
+
+        return output
+      end
+
+      # 修飾決定表(1D6)
+      def mk_decoration_table(num)
+        debug("mk_decoration_table num", num)
+
+        table = [
+          [1, lambda { mk_basic_decoration_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [2, lambda { mk_spooky_decoration_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [3, lambda { mk_katakana_decoration_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # 地名決定表(1D6)
+      def mk_placename_table(num)
+        table = [
+          [1, lambda { mk_passage_placename_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [2, lambda { mk_natural_placename_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+          [3, lambda { mk_artifact_placename_table(@randomizer.roll_d66(D66SortType::ASC)) }],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # 基本表(D66)
+      def mk_basic_decoration_table(num)
+        table = [
+          [11, "欲望（よくぼう）"],
+          [12, "漂流（ひょうりゅう）"],
+          [13, "黄金（おうごん）"],
+          [14, "火達磨（ひだるま）"],
+          [15, "災厄（さいやく）"],
+          [16, "三日月（みかづき）"],
+          [22, "絡繰り（からくり）"],
+          [23, "流星（りゅうせい）"],
+          [24, "棘々（とげとげ）"],
+          [25, "鏡（かがみ）"],
+          [26, "銀鱗（ぎんりん）"],
+          [33, "螺旋（らせん）"],
+          [34, "七色（なないろ）"],
+          [35, "殉教（じゅんきょう）"],
+          [36, "水晶（すいしょう）"],
+          [44, "氷結（ひょうけつ）"],
+          [45, "忘却（ぼうきゃく）"],
+          [46, "幸福（こうふく）"],
+          [55, "妖精（ようせい）"],
+          [56, "霧雨（きりさめ）"],
+          [66, "夕暮れ（ゆうぐれ）"],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # 不気味表(D66)
+      def mk_spooky_decoration_table(num)
+        table = [
+          [11, "赤錆（あかさび）"],
+          [12, "串刺し（くしざし）"],
+          [13, "鬼蜘蛛（おにぐも）"],
+          [14, "蠍（さそり）"],
+          [15, "幽霊（ゆうれい）"],
+          [16, "髑髏（どくろ）"],
+          [22, "血溜まり（ちだまり）"],
+          [23, "臓物（ぞうもつ）"],
+          [24, "骸（むくろ）"],
+          [25, "鉤爪（かぎづめ）"],
+          [26, "犬狼（けんろう）"],
+          [33, "奈落（ならく）"],
+          [34, "大蛇（おろち）"],
+          [35, "地獄（じごく）"],
+          [36, "蚯蚓（みみず）"],
+          [44, "退廃（たいはい）"],
+          [45, "土竜（もぐら）"],
+          [46, "絶望（ぜつぼう）"],
+          [55, "夜泣き（よなき）"],
+          [56, "緑林（りょくりん）"],
+          [66, "どん底（どんぞこ）"],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # カタカナ表(D66)
+      def mk_katakana_decoration_table(num)
+        table = [
+          [11, "マヨネーズ"],
+          [12, "ダイナマイト"],
+          [13, "ドラゴン"],
+          [14, "ボヨヨン"],
+          [15, "モケモケ"],
+          [16, "マヌエル"],
+          [22, "ダイス"],
+          [23, "ロマン"],
+          [24, "ウクレレ"],
+          [25, "エップカプ"],
+          [26, "カンパネルラ"],
+          [33, "マンチキン"],
+          [34, "バロック"],
+          [35, "ミサイル"],
+          [36, "ドッキリ"],
+          [44, "ブラック"],
+          [45, "好きなモンスターの名前"],
+          [46, "好きなトラップの名前"],
+          [55, "好きな単語表で"],
+          [56, "好きな名前決定表で"],
+          [66, "好きな数字の組み合わせ"],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # 通路系地名表(D66)
+      def mk_passage_placename_table(num)
+        table = [
+          [11, "門（ゲート）"],
+          [12, "回廊（コリドー）"],
+          [13, "通り（ストリート）"],
+          [14, "小路（アレイ）"],
+          [15, "大路（アベニュー）"],
+          [16, "街道（ロード）"],
+          [22, "鉄道（ライン）"],
+          [23, "迷宮（メイズ）"],
+          [24, "坑道（トンネル）"],
+          [25, "坂（スロープ）"],
+          [26, "峠（パス）"],
+          [33, "運河（カナル）"],
+          [34, "水路（チャネル）"],
+          [35, "河（ストリーム）"],
+          [36, "堀（モート）"],
+          [44, "溝（ダイク）"],
+          [45, "階段（ステア）"],
+          [46, "辻（トレイル）"],
+          [55, "橋（ブリッジ）"],
+          [56, "穴（ホール）"],
+          [66, "柱廊（ストア）"],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # 自然系地名表(D66)
+      def mk_natural_placename_table(num)
+        table = [
+          [11, "砂漠（デザート）"],
+          [12, "丘陵（ヒル）"],
+          [13, "海（オーシャン）"],
+          [14, "森（フォレスト）"],
+          [15, "沼（ポンド）"],
+          [16, "海岸（コースト）"],
+          [22, "密林（ジャングル）"],
+          [23, "湖（レイク）"],
+          [24, "山脈（マウンテンズ）"],
+          [25, "平原（プレイン）"],
+          [26, "ヶ原（ランド）"],
+          [33, "荒野（ヒース）"],
+          [34, "渓谷（ヴァレー）"],
+          [35, "島（アイランド）"],
+          [36, "連峰（ピークス）"],
+          [44, "火山（ヴォルケイノ）"],
+          [45, "湿原（ウェットランド）"],
+          [46, "星雲（ネビュラ）"],
+          [55, "星（スター）"],
+          [56, "ヶ淵（プール）"],
+          [66, "雪原（スノウズ）"],
+        ]
+        return get_table_by_number(num, table)
+      end
+
+      # 人工系地名表(D66)
+      def mk_artifact_placename_table(num)
+        table = [
+          [11, "城（キャッスル）"],
+          [12, "壁（ウォール）"],
+          [13, "砦（フォート）"],
+          [14, "地帯（ゾーン）"],
+          [15, "室（ルーム）"],
+          [16, "の間（チャンバー）"],
+          [22, "浴室（バス）"],
+          [23, "畑（ファーム）"],
+          [24, "館（ハウス）"],
+          [25, "座（コンスティレィション）"],
+          [26, "遺跡（ルイン）"],
+          [33, "ヶ浜（ビーチ）"],
+          [34, "塔（タワー）"],
+          [35, "墓場（グレイブ）"],
+          [36, "洞（ケイヴ）"],
+          [44, "堂（バジリカ）"],
+          [45, "野（フィールド）"],
+          [46, "書院（スタディ）"],
+          [55, "駅前（ステイション）"],
+          [56, "房（クラスター）"],
+          [66, "腐海（ケイオスシー）"],
+        ]
+        return get_table_by_number(num, table)
+      end
+    end
+  end
+end
