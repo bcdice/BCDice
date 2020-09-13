32b57c3ef133dca79177067d2ecb6af835596f76
SAKATA Sinji <ysakasin@gmail.com>
2020-09-14 00:15:03 +0900

    計算コマンドでカッコがなくても四則演算ができるようにする

--- lib/bcdice/common_command/calc.rb
+++ lib/bcdice/common_command/calc.rb
@@ -1,24 +1,35 @@
 module BCDice
   module CommonCommand
     class Calc
-      def initialize(command, _randomizer, _game_system)
+      # @param command [String]
+      # @param _randomizer [Randomizer]
+      # @param game_system [Base]
+      def initialize(command, _randomizer, game_system)
         @command = command
+        @game_system = game_system
 
         @is_secret = false
       end
 
+      # @return [Boolean]
       def secret?
         @is_secret
       end
 
+      # @return [String, nil]
       def eval
-        m = /^(S)?C(-?\d+)$/i.match(@command)
+        m = %r{^(S)?C([\+\-\*/\d\(\)]+)$}i.match(@command)
         unless m
           return nil
         end
 
         @is_secret = !m[1].nil?
-        value = m[2].to_i
+        expr = ArithmeticEvaluator.new(m[2], round_type: @game_system.round_type)
+        value = expr.eval()
+        if expr.error?
+          return nil
+        end
+
         return ": 計算結果 ＞ #{value}"
       end
     end
