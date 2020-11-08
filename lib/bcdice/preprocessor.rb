ef1e3beec3ce389d98aab8a3043f216cd095f5a8
SAKATA Sinji <ysakasin@gmail.com>
2020-11-08 21:19:46 +0900

    事前処理の乱数記法を廃止する
    `[1...6]` や `[1D6]` と入力すると前処理で乱数を用いて展開されていた。
    この機能はマニュアルにも載っていない機能で、用途もほとんどないので削除する。

--- lib/bcdice/preprocessor.rb
+++ lib/bcdice/preprocessor.rb
@@ -3,31 +3,26 @@ module BCDice
   #
   # @example
   #   Preprocessor.process(
-  #     "1d6+[2D6]+[40..50]+4D+(3*4) 切り取られる部分",
-  #     randomizer,
+  #     "1d6+4D+(3*4) 切り取られる部分",
   #     game_system
-  #   ) #=> "1d6+3+46+4D6+7"
+  #   ) #=> "1d6+4D6+7"
   class Preprocessor
     # @param (see #initialize)
     # @return [String]
-    def self.process(text, randomizer, game_system)
-      Preprocessor.new(text, randomizer, game_system).process()
+    def self.process(text, game_system)
+      Preprocessor.new(text, game_system).process()
     end
 
     # @param text [String]
-    # @param randomizer [Randomizer]
     # @param game_system [Base]
-    def initialize(text, randomizer, game_system)
+    def initialize(text, game_system)
       @text = text
-      @randomizer = randomizer
       @game_system = game_system
     end
 
     # @return [String]
     def process
       trim_after_whitespace()
-      replace_preroll()
-      replace_range()
       replace_parentheses()
 
       @text = @game_system.change_text(@text)
@@ -42,34 +37,6 @@ module BCDice
     # 空白より前だけを取る
     def trim_after_whitespace()
       @text = @text.strip.split(/\s/, 2).first
-    end
-
-    # [1D6]のような部分を事前ダイスロールする
-    def replace_preroll()
-      @text = @text.gsub(/\[\d+D\d+\]/i) do |matched|
-        # Remove '[' and ']'
-        command = matched[1..-2].upcase
-        times, sides = command.split("D").map(&:to_i)
-
-        @randomizer.roll_sum(times, sides)
-      end
-    end
-
-    # [1...6]のような部分を事前ダイスロールする
-    def replace_range
-      @text = @text.gsub(/\[(\d+)\.\.\.(\d+)\]/) do |matched|
-        first = Regexp.last_match(1).to_i
-        last = Regexp.last_match(2).to_i
-
-        if first > last
-          matched
-        else
-          steps = last - first + 1
-          dice = @randomizer.roll_once(steps)
-
-          first + dice - 1
-        end
-      end
     end
 
     # カッコ書きの数式を事前計算する
