9d5b566e54873257fef046586a0fde140d726dcb
SAKATA Sinji <ysakasin@gmail.com>
2020-11-08 12:08:02 +0900

    Calc.#eval で処理を完結させる

--- lib/bcdice/common_command/calc.rb
+++ lib/bcdice/common_command/calc.rb
@@ -1,51 +1,24 @@
 module BCDice
   module CommonCommand
-    class Calc
+    module Calc
       PREFIX_PATTERN = /C/.freeze
 
       class << self
-        def eval(command, game_system, randomizer)
-          command = new(command, randomizer, game_system)
-          res = command.eval()
-          return nil unless res
+        def eval(command, game_system, _randomizer)
+          m = %r{^(S)?C([\+\-\*/\d\(\)]+)$}i.match(command)
+          return nil unless m
+
+          expr = ArithmeticEvaluator.new(m[2], round_type: game_system.round_type)
+          value = expr.eval()
+          if expr.error?
+            return nil
+          end
 
           Result.new.tap do |r|
-            r.secret = command.secret?
-            r.text = res
+            r.secret = !m[1].nil?
+            r.text = "計算結果 ＞ #{value}"
           end
         end
-      end
-
-      # @param command [String]
-      # @param _randomizer [Randomizer]
-      # @param game_system [Base]
-      def initialize(command, _randomizer, game_system)
-        @command = command
-        @game_system = game_system
-
-        @is_secret = false
-      end
-
-      # @return [Boolean]
-      def secret?
-        @is_secret
-      end
-
-      # @return [String, nil]
-      def eval
-        m = %r{^(S)?C([\+\-\*/\d\(\)]+)$}i.match(@command)
-        unless m
-          return nil
-        end
-
-        @is_secret = !m[1].nil?
-        expr = ArithmeticEvaluator.new(m[2], round_type: @game_system.round_type)
-        value = expr.eval()
-        if expr.error?
-          return nil
-        end
-
-        return "計算結果 ＞ #{value}"
       end
     end
   end
