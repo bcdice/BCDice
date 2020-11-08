a843c0dd7313ec26e5ddd01eba5af73feeb96d6c
SAKATA Sinji <ysakasin@gmail.com>
2020-11-08 16:53:50 +0900

    Versionコマンドをリファクタリング

--- lib/bcdice/common_command/version.rb
+++ lib/bcdice/common_command/version.rb
@@ -1,37 +1,15 @@
 module BCDice
   module CommonCommand
-    class Version
+    module Version
       PREFIX_PATTERN = /BCDiceVersion/.freeze
 
       class << self
-        def eval(command, game_system, randomizer)
-          command = new(command, randomizer, game_system)
-          res = command.eval()
-          return nil unless res
-
-          Result.new.tap do |r|
-            r.secret = command.secret?
-            r.text = res
+        def eval(command, _game_system, _randomizer)
+          if command == "BCDICEVERSION"
+            Result.new.tap do |r|
+              r.text = "BCDice Ver#{BCDice::VERSION}"
+            end
           end
-        end
-      end
-
-      # @param command [String]
-      # @param _randomizer [Randomizer]
-      # @param _game_system [Base]
-      def initialize(command, _randomizer, _game_system)
-        @command = command
-      end
-
-      # @return [Boolean]
-      def secret?
-        false
-      end
-
-      # @return [String, nil]
-      def eval
-        if @command == "BCDICEVERSION"
-          "BCDice Ver#{BCDice::VERSION}"
         end
       end
     end
