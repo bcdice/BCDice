9a27811e5b429e761e2addd3711ac9826dc99563
saronpasu <jamneco@gmail.com>
2021-01-03 20:28:31 +0900

    DesperateRunTRPG と ジキルとハイドとグリトグラのダイスボット、テストコードを追加。

---
+++ lib/bcdice/game_system/DesperateRun.rb
@@ -0,0 +1,171 @@
+# frozen_string_literal: true
+
+module BCDice
+  module GameSystem
+    class DesperateRun < Base
+      # ゲームシステムの識別子
+      ID = "DesperateRun"
+
+      # ゲームシステム名
+      NAME = "Desperate Run"
+
+      # ゲームシステム名の読みがな
+      SORT_KEY = "てすへれいとらん"
+
+      # ダイスボットの使い方
+      HELP_MESSAGE = <<~INFO_MESSAGE_TEXT
+        ・難易度算出コマンド　DDC
+        ・判定コマンド　RCx　or　RCx+y　or　RCx-y（x＝難易度、y=修正値（省略可能））
+        ・アクシデント表　ACT
+        ・初期アイテム表　ITEMT
+        ・動機表　DOUKIT
+        ・死亡フラグ台詞表　FLAGT
+        ・途中参加動機表　ENTRYT
+        ・途中参加道中表　ROADT
+        Desperate Run TRPG　byかむらい
+      INFO_MESSAGE_TEXT
+
+      TABLES = {
+        "ACT" => DiceTable::Table.new("アクシデント表", "1D6", [
+          "ＰＣ全員、隊列を１Ｄ６で決める（１２：前列、３４：中列、５６：後列）",
+          "１ターン、モンスター側のみ行動する。",
+          "この戦闘の行動順が、ＰＣ→モンスター、から、モンスター→ＰＣに変わる。",
+          "ＰＣ全員、アイテム１つを選んで失う。",
+          "ＰＣ全員１Ｄ６を振る。この戦闘中、その出目はノープランになる。",
+          "ＰＣ全員、Ｆｌａｇが１増加する。"
+        ]),
+        "ITEMT" => DiceTable::Table.new("初期アイテム表", "2D6", [
+          "ロケットランチャー　効果：（装備）マカブルＬｖ＋３　回数：攻撃１回",
+          "キーピック　効果：（消費）ムーブシーンで使用可。判定の出目の合計＋１　回数：２回",
+          "レーダー　効果：（装備）ノープラン　装備時、「危険」を１減らすことが出来る　回数：危険減少２回",
+          "食料　効果：（消費）ムーブシーンで使用可。Ｌｉｆｅ３回復　回数：１回",
+          "応急キット　効果：（装備）メディカルＬｖ＋１　回数：回復３回",
+          "刃物　効果：（装備）アタックＬｖ＋１　回数：攻撃６回",
+          "銃　効果：（装備）シュートＬｖ＋１　回数：攻撃６回",
+          "ドラッグ　効果：（消費）いつでも使用可。Ｂｒａｖｅ１回復　回数：１回",
+          "金券　効果：（消費）アフタープレイで使用可。経験点＋２　回数：１回",
+          "プロテクター　効果：（装備）ガードＬｖ＋１　回数：防御５回",
+          "トミーガン　効果：（装備）チェーンＬｖ＋５　回数：攻撃１回"
+        ]),
+        "DOUKIT" => DiceTable::Table.new("動機表", "1D6", [
+          "隷属。何か弱みに付け込まれ、嫌々ながらも参加することとなる。後ろめたい事、借金、など。",
+          "献身。この番組は誰かのために出ている。病気の家族を救うため、参加者の一人が恋人である、など。",
+          "成行。望んでいないのに参加することとなってしまった。誰かが勝手に申し込んだ、紛れ込んでしまった、など。",
+          "渇望。とにかく何かを欲して参加している。金、スリル、金で手に入るもの、など。",
+          "奇人。好き好んでこの番組に出ている。殺人癖、ナルシスト、自殺願望、化け物マニア、など。",
+          "仕事。この番組に出るのは仕事だからだ。賞金稼ぎ、芸能人、記者、番組スタッフ、など。"
+        ]),
+        "FLAGT" => DiceTable::Table.new("死亡フラグ台詞表", "1D6", [
+          "希望。「これが終わったら、一緒に酒でも飲もうや」「もう何も怖くない」など。",
+          "望郷。「くそっ、、、俺には、帰りを待つ人が、、、っ！」「ああ、故郷のマルゲリータをもう一度食べたかった、、、」など。",
+          "狂乱。「お前を、殺せば、俺は、億万長者なんだよぉぉぉお！！」「ヒハッ、死ね死ね死ね死ねぇぇぇぇ」など。",
+          "絶望。「もうだめだぁ、、、おしまいだぁ、、、」「く、くるなっ、くるなぁぁぁぁ！！」など。",
+          "慢心。「なんだ、こんなやつ、俺だけで十分だ」「大丈夫だ、問題ない」など。",
+          "犠牲。「ふふ、俺なら大丈夫だ、、、気にするな」「お前ら下がれっ！ここは俺に任せろ！」など。"
+        ]),
+        "ENTRYT" => DiceTable::Table.new("途中参加動機表", "1D6", [
+          "遅刻。もともと参加する予定だったのだが遅れてしまった。今からでも走れと無理矢理参加。",
+          "現住。もともとここに住んでいた。何？番組？え？あ、お金もらえんの？イイネ！",
+          "突発。もともと視聴者としてスタジオに居たんだけど、司会にうまく乗せられて・・・",
+          "乱入。何か目的があって乱入した。今回のステージが簡単に見えたり、参加者に大事な人がいたのかもしれない。",
+          "神隠。今回より前の番組に参加していたが行方不明・死んだと思われていた。が。君はまだここにいる。",
+          "職員。あれ、逃げ遅れました？あらあら、大変ですね、しょうがないから参加者さんと一緒に走ってください。"
+        ]),
+        "ROADT" => DiceTable::Table.new("途中参加道中表", "1D6", [
+          "死線。死ぬかと思った。Ｆｌａｇ＋１",
+          "怪我。痛い。Ｌｉｆｅ－１",
+          "失意。どうしてこうなった。Ｂｒａｖｅ－１。減らせない場合、Ｆｌａｇ＋１",
+          "紛失。どっかいった。Ｉｔｅｍをどれか１つ捨てる。捨てれない場合、Ｆｌａｇ＋１",
+          "追尾。後ろ！後ろーっ！参加開始部屋の危険＋１",
+          "迷子。あれ、ここどこだ？途中参加道中表を振る回数＋２"
+        ])
+      }.freeze
+
+      register_prefix('(RC\d+|DDC|ACT|ITEMT|DOUKIT|FLAGT|ENTRYT|ROADT).*')
+
+      def initialize(command)
+        super(command)
+        @sortType = true
+        @d66Type = D66SortType::ASC
+      end
+
+      def eval_game_system_specific_command(command)
+        debug("eval_game_system_specific_command begin string", command)
+
+        result = checkRoll(command)
+        return result unless result.empty?
+
+        debug("各種表として処理")
+        return rollTableCommand(command)
+      end
+
+      def checkRoll(string)
+        debug("checkRoll begin string", string)
+
+        return "" unless /^RC(\d+)(([+]|-)(\d+))?$/i =~ string
+
+        target = Regexp.last_match(1).to_i
+        operator = Regexp.last_match(3)
+        value = Regexp.last_match(4).to_i
+        modify = 0
+
+        result = "判定　難易度：#{target}　"
+
+        unless operator.nil?
+          modify = value if operator == "+"
+          modify = value * -1 if operator == "-"
+          result += "修正値：#{modify}　"
+        end
+
+        d1, d2 = @randomizer.roll_barabara(2, 6)
+
+        result += "＞　出目：#{d1}、#{d2}　＞　"
+
+        if d1 == d2
+          result += "ゾロ目！【Ｃｒｉｔｉｃａｌ】"
+        elsif d1 + d2 == 7
+          result += "ダイスの出目が表裏！【Ｆｕｍｂｌｅ】"
+        elsif d1 + d2 + modify >= target
+          result += "#{d1 + d2 + modify}、難易度以上！【Ｓｕｃｃｅｓｓ】"
+        else
+          result += "#{d1 + d2 + modify}、難易度未満！【Ｍｉｓｓ】"
+        end
+
+        return result
+      end
+
+      ####################
+      # 各種表
+
+      def rollTableCommand(command)
+        command = command.upcase
+
+        debug("rollDiceCommand command", command)
+
+        if command == "DDC"
+          return choiceddcTable()
+        end
+
+        roll_tables(command, TABLES)
+      end
+
+      # ##呼び出し＋α
+
+      def choiceddcTable
+        name = "難易度決定"
+        text = "#{name}："
+
+        d1, d2 = @randomizer.roll_barabara(2, 6)
+        text += "　出目：#{d1}、#{d2}　＞　"
+
+        if d1 >= d2
+          text += "#{d1}－#{d2}＝#{d1 - d2}　＞　難易度#{5 + (d1 - d2)}"
+        else
+          text += "#{d2}－#{d1}＝#{d2 - d1}　＞　難易度#{5 + (d2 - d1)}"
+        end
+
+        return text
+      end
+    end
+  end
+end
