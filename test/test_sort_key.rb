56c2a71cd9af6cd131edb133d8668f1c6c4ce510
SAKATA Sinji <ysakasin@gmail.com>
2021-03-20 22:30:49 +0900

    正規表現を修正
    Co-authored-by: Kosuke Yamashita <ochaochaocha3@gmail.com>

--- test/test_sort_key.rb
+++ test/test_sort_key.rb
@@ -5,14 +5,14 @@ require "bcdice"
 require "bcdice/game_system"
 
 class TestSortKey < Test::Unit::TestCase
-  HIRAGANA_BASIC_CHARS = "[あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん]"
+  HIRAGANA_BASIC_CHARS = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん"
 
   data do
     BCDice.all_game_systems.map { |s| [s::ID, s::SORT_KEY] }.to_h
   end
   def test_sort_key(sort_key)
     assert_match(
-      /\A(?:#{HIRAGANA_BASIC_CHARS}+[\d.A-Za-z]*|国際化:[\w ]+:.+|\*たいすほつと)\z/,
+      /\A(?:[#{HIRAGANA_BASIC_CHARS}][\d.A-Za-z#{HIRAGANA_BASIC_CHARS}]*|国際化:[\w ]+:.+|\*たいすほつと)\z/,
       sort_key,
       "SORT_KEY \"#{sort_key}\" はSORT_KEYの規約に違反しています。規約は docs/dicebot_sort_key.md を参照してください"
     )
