ae19e131ea4a442f34a69c022d9ac633c08470de
SAKATA Sinji <ysakasin@gmail.com>
2021-03-20 15:01:50 +0900

    コードの微修正

--- lib/bcdice/game_system/AngelGear.rb
+++ lib/bcdice/game_system/AngelGear.rb
@@ -11,10 +11,10 @@ module BCDice
       ID = 'AngelGear'
 
       # ゲームシステム名
-      NAME = 'エンゼルギア2nd'
+      NAME = 'エンゼルギア 天使大戦TRPG The 2nd Editon'
 
       # ゲームシステム名の読みがな
-      SORT_KEY = 'エンセルキア'
+      SORT_KEY = 'エンセルキア2'
 
       # ダイスボットの使い方
       HELP_MESSAGE = <<~MESSAGETEXT
@@ -33,17 +33,13 @@ module BCDice
         super(command)
 
         @sort_barabara_dice = true # バラバラロール（Bコマンド）でソート有
-        @d66_sort_type = D66SortType::NO_SORT
       end
 
       def eval_game_system_specific_command(command)
-        command = command.upcase
-        if TABLES.key?(command)
-          return roll_tables(command, TABLES)
-        elsif (m = /(\d+)AG(\d+)?(([+\-]\d+)*)/.match(command))
-          return resolute_action(m[1].to_i, m[2] && m[2].to_i, m[3], command)
+        if (m = /^(\d+)AG(\d+)?(([+\-]\d+)*)$/.match(command))
+          resolute_action(m[1].to_i, m[2]&.to_i, m[3], command)
         else
-          return nil
+          roll_tables(command, TABLES)
         end
       end
 
@@ -53,11 +49,9 @@ module BCDice
         modify_n = 0
         success = 0
 
-        unless skill_value.nil?
+        if skill_value
           success = dice.count { |val| val <= skill_value }
-          unless  modify.empty?
-            modify_n = Arithmetic.eval(modify, RoundType::FLOOR)
-          end
+          modify_n = Arithmetic.eval(modify, RoundType::FLOOR) unless modify.empty?
         end
 
         output = "(#{command}) ＞ #{success}[#{dice_text}]#{format('%+d', modify_n)} ＞ 成功数: #{success + modify_n}"
@@ -67,55 +61,61 @@ module BCDice
         return output
       end
 
-      TABLES =
-        {
-          'ET' => DiceTable::D66GridTable.new(
-            '感情表',
+      TABLES = {
+        'ET' => DiceTable::D66GridTable.new(
+          '感情表',
+          [
             [
-              ['好奇心（好奇心）',
-               '憧れ（あこがれ）',
-               '尊敬（そんけい）',
-               '仲間意識（なかまいしき）',
-               '母性愛（ぼせいあい）',
-               '感心（かんしん）'],
-
-              ['純愛（じゅんあい）',
-               '友情（ゆうじょう）',
-               '同情（どうじょう）',
-               '父性愛（ふせいあい）',
-               '幸福感（こうふくかん）',
-               '信頼（しんらい）'],
-
-              ['競争心（きょうそうしん）',
-               '親近感（しんきんかん）',
-               'まごころ',
-               '好意（こうい）',
-               '有為（ゆうい）',
-               '崇拝（すうはい）'],
-
-              ['大嫌い（だいきらい）',
-               '妬み（ねたみ）',
-               '侮蔑（ぶべつ）',
-               '腐れ縁（くされえん）',
-               '恐怖（きょうふ）',
-               '劣等感（れっとうかん）'],
-
-              ['偏愛（へんあい）',
-               '寂しさ（さびしさ）',
-               '憐憫（れんびん）',
-               '闘争心（とうそうしん）',
-               '食傷（しょくしょう）',
-               '嘘つき（うそつき）'],
-
-              ['甘え（あまえ）',
-               '苛立ち（いらだち）',
-               '下心（したごころ）',
-               '憎悪（ぞうお）',
-               '疑惑（ぎわく）',
-               '支配（しはい）'],
-            ]
-          )
-        }.freeze
+              '好奇心（好奇心）',
+              '憧れ（あこがれ）',
+              '尊敬（そんけい）',
+              '仲間意識（なかまいしき）',
+              '母性愛（ぼせいあい）',
+              '感心（かんしん）'
+            ],
+            [
+              '純愛（じゅんあい）',
+              '友情（ゆうじょう）',
+              '同情（どうじょう）',
+              '父性愛（ふせいあい）',
+              '幸福感（こうふくかん）',
+              '信頼（しんらい）'
+            ],
+            [
+              '競争心（きょうそうしん）',
+              '親近感（しんきんかん）',
+              'まごころ',
+              '好意（こうい）',
+              '有為（ゆうい）',
+              '崇拝（すうはい）'
+            ],
+            [
+              '大嫌い（だいきらい）',
+              '妬み（ねたみ）',
+              '侮蔑（ぶべつ）',
+              '腐れ縁（くされえん）',
+              '恐怖（きょうふ）',
+              '劣等感（れっとうかん）'
+            ],
+            [
+              '偏愛（へんあい）',
+              '寂しさ（さびしさ）',
+              '憐憫（れんびん）',
+              '闘争心（とうそうしん）',
+              '食傷（しょくしょう）',
+              '嘘つき（うそつき）'
+            ],
+            [
+              '甘え（あまえ）',
+              '苛立ち（いらだち）',
+              '下心（したごころ）',
+              '憎悪（ぞうお）',
+              '疑惑（ぎわく）',
+              '支配（しはい）'
+            ],
+          ]
+        )
+      }.freeze
 
       register_prefix('(\d+)AG(\d+)?(([\+\-]\d+)*)', TABLES.keys)
     end
