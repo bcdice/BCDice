2b8d13f628597c28ce779eede3a2f63fc53e85d9
SAKATA Sinji <ysakasin@gmail.com>
2021-04-16 18:00:31 +0900

    [Elric] result_1d100を使う

--- lib/bcdice/game_system/Elric.rb
+++ lib/bcdice/game_system/Elric.rb
@@ -16,20 +16,24 @@ module BCDice
       HELP_MESSAGE = "貫通、クリティカル、ファンブルの自動判定を行います。\n"
 
       # ゲーム別成功度判定(1d100)
-      def check_1D100(total, _dice_total, cmp_op, target)
-        return '' if target == '?'
-        return '' unless cmp_op == :<=
+      def result_1d100(total, _dice_total, cmp_op, target)
+        return nil unless cmp_op == :<=
 
-        # 1は常に貫通
-        return " ＞ 貫通" if total <= 1
-        # 100は常に致命的失敗
-        return " ＞ 致命的失敗" if total >= 100
-
-        return " ＞ 決定的成功" if total <= (target / 5 + 0.9)
-        return " ＞ 成功" if total <= target
-        return " ＞ 致命的失敗" if (total >= 99) && (target < 100)
-
-        return " ＞ 失敗"
+        if total <= 1
+          Result.critical("貫通") # 1は常に貫通
+        elsif total >= 100
+          Result.fumble("致命的失敗")  # 100は常に致命的失敗
+        elsif target == '?'
+          Result.nothing
+        elsif total <= (target / 5.0).ceil
+          Result.critical("決定的成功")
+        elsif total <= target
+          Result.success("成功")
+        elsif (total >= 99) && (target < 100)
+          Result.fumble("致命的失敗")
+        else
+          Result.failure("失敗")
+        end
       end
     end
   end
