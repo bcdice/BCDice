e16df5738edd0e5707a27fb511447c5f78332e2d
SAKATA Sinji <ysakasin@gmail.com>
2021-04-17 18:17:16 +0900

    [RuneQuest] result_1d100を使う

--- lib/bcdice/game_system/RuneQuest.rb
+++ lib/bcdice/game_system/RuneQuest.rb
@@ -18,8 +18,8 @@ module BCDice
       INFO_MESSAGE_TEXT
 
       # ゲーム別成功度判定(1d100)
-      def check_1D100(total, _dice_total, cmp_op, target)
-        return '' if target == '?'
+      def result_1d100(total, _dice_total, cmp_op, target)
+        return Result.nothing if target == '?'
         return nil unless cmp_op == :<=
 
         # RuneQuest QUICK-START RULESを元に修正
@@ -28,18 +28,18 @@ module BCDice
 
         if (total <= 1) || (total <= critical_value)
           # 1は常に決定的成功
-          " ＞ 決定的成功"
+          Result.critical("決定的成功")
         elsif total >= 100
           # 100は常に致命的失敗
-          " ＞ 致命的失敗"
+          Result.fumble("致命的失敗")
         elsif total <= (target.to_f / 5).round
-          " ＞ 効果的成功"
+          Result.success("効果的成功")
         elsif total <= target
-          " ＞ 成功"
+          Result.success("成功")
         elsif total >= 95 + critical_value
-          " ＞ 致命的失敗"
+          Result.fumble("致命的失敗")
         else
-          " ＞ 失敗"
+          Result.failure("失敗")
         end
       end
     end
