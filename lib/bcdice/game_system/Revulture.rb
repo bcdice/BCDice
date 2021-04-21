33b58ef518557810f3520031eb6cd9a1b2d2b476
ViVi <vivi.shark.tech+github@gmail.com>
2021-04-21 17:20:13 +0900

    [Revulture] 【アタック判定】のシンプルなダイスロールを追加

--- lib/bcdice/game_system/Revulture.rb
+++ lib/bcdice/game_system/Revulture.rb
@@ -13,7 +13,28 @@ module BCDice
       SORT_KEY = 'こうさいのりうあるちやあ'
 
       # ダイスボットの使い方
-      HELP_MESSAGE = "※このダイスボットは部屋のシステム名表示用となります。\n"
+      HELP_MESSAGE = <<~HELP
+        ■アタック判定（ xAT, xATK, xATTACK ）
+        x: ダイス数
+        例） 3AT, 4ATK, 5ATTACK
+      HELP
+
+      ATTACK_ROLL_REG = /^(\d+)?AT(TACK|K)?/i.freeze
+      register_prefix('\d+AT')
+
+      def eval_game_system_specific_command(command)
+        if (m = ATTACK_ROLL_REG.match(command))
+          roll_attack(m[1].to_i)
+        end
+      end
+
+      def roll_attack(dice_count)
+        raise if dice_count < 1
+
+        dices = @randomizer.roll_barabara(dice_count, 6).sort
+
+        Result.new("(#{dice_count}attack) ＞ #{dices.join(',')}")
+      end
     end
   end
 end
