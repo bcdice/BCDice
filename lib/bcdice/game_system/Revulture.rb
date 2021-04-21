7447e69436b3cc92270d51299b9b74661ecfba28
ViVi <vivi.shark.tech+github@gmail.com>
2021-04-21 19:09:23 +0900

    [Revulture] 【アタック判定】のダイス数に加算式を指定できるように

--- lib/bcdice/game_system/Revulture.rb
+++ lib/bcdice/game_system/Revulture.rb
@@ -15,20 +15,21 @@ module BCDice
       # ダイスボットの使い方
       HELP_MESSAGE = <<~HELP
         ■アタック判定（ xAT, xATK, xATTACK ）
-        x: ダイス数
-        例） 3AT, 4ATK, 5ATTACK
+        x: ダイス数（加算記号 + で連結可能）
+        例） 3AT, 4ATK, 5+6ATTACK
       HELP
 
-      ATTACK_ROLL_REG = /^(\d+)?AT(TACK|K)?/i.freeze
-      register_prefix('\d+AT')
+      ATTACK_ROLL_REG = /^(\d+(\+\d+)*)?AT(TACK|K)?/i.freeze
+      register_prefix('\d+(\+\d+)*AT')
 
       def eval_game_system_specific_command(command)
         if (m = ATTACK_ROLL_REG.match(command))
-          roll_attack(m[1].to_i)
+          roll_attack(m[1])
         end
       end
 
-      def roll_attack(dice_count)
+      def roll_attack(dice_count_expression)
+        dice_count = Arithmetic.eval(dice_count_expression, round_type: RoundType::FLOOR)
         raise if dice_count < 1
 
         dices = @randomizer.roll_barabara(dice_count, 6).sort
