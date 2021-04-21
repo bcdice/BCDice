86e0cdb71cc09d61dd6853b117395b20934068b7
ViVi <vivi.shark.tech+github@gmail.com>
2021-04-21 21:13:44 +0900

    [Revulture] 【アタック判定】に、追加ダメージの規則を複数同時に指定できるように
    ルールブックp67「ファーボル・ドラゴンブレス」の効果などを想定。

--- lib/bcdice/game_system/Revulture.rb
+++ lib/bcdice/game_system/Revulture.rb
@@ -23,15 +23,22 @@ module BCDice
         y: 目標値（ 1 以上 6 以下。加算 + を使用可能）
         例） 3AT<=4, 3AT<=2+1
 
-        □アタック判定　目標値＆追加ダメージつき（ xAT<=y[>=a:+b], xATK<=y[>=a:+b], xATTACK<=y[>=a:+b] ）
+        □アタック判定　目標値＆追加ダメージつき（ xAT<=y[>=a:+b], xATK<=y[>=a:+b], xATTACK<=y[z] ）
         x: ダイス数（加算 + と除算 / を使用可能）
         y: 目標値（ 1 以上 6 以下。加算 + を使用可能）
-        a: ヒット数が a 以上なら
+        z: 追加ダメージの規則（詳細は後述）（※複数同時に指定可能）
+
+        ▽追加ダメージの規則 [a:+b]
+        a: ヒット数が a なら
+        　=a　（ヒット数が a に等しい）
+        　>=a　（ヒット数が a 以上）
         b: ダメージを b 点追加
+
         例） 3AT<=4[>=2:+3] #ルールブックp056「グレングラントAR」
+        例） 2AT<=4[=1:+5][>=2:+8] #ルールブックp067「ファーボル・ドラゴンブレス」
       HELP
 
-      ATTACK_ROLL_REG = %r{^(\d+([+/]\d+)*)?AT(TACK|K)?(<=([1-6](\+\d)*))?(\[>?=\d+:\+\d+\])?}i.freeze
+      ATTACK_ROLL_REG = %r{^(\d+([+/]\d+)*)?AT(TACK|K)?(<=([1-6](\+\d)*))?((\[>?=\d+:\+\d+\])+)?}i.freeze
       register_prefix('\d+([+\/]\d+)*AT')
 
       def eval_game_system_specific_command(command)
@@ -40,7 +47,7 @@ module BCDice
         end
       end
 
-      def roll_attack(dice_count_expression, border_expression, additional_damage_rule)
+      def roll_attack(dice_count_expression, border_expression, additional_damage_rules)
         dice_count = Arithmetic.eval(dice_count_expression, round_type: RoundType::FLOOR)
         raise if dice_count < 1
 
@@ -49,17 +56,18 @@ module BCDice
         dices = @randomizer.roll_barabara(dice_count, 6).sort
         critical_hit_count = dices.count { |dice| dice == 1 }
         hit_count = border.nil? ? nil : dices.count { |dice| dice <= border.to_i } + critical_hit_count
-        damage = additional_damage_rule.nil? ? nil : hit_count.to_i
+        damage = additional_damage_rules.nil? ? nil : hit_count.to_i
 
-        if !damage.nil? && !additional_damage_rule.nil?
-          rule = self.class.parse_additional_damage_rule(additional_damage_rule)
-          if rule[:condition].call(hit_count)
-            damage += rule[:additinal_damage]
+        if !damage.nil? && !additional_damage_rules.nil?
+          self.class.parse_additional_damage_rules(additional_damage_rules).each do |rule|
+            if rule[:condition].call(hit_count)
+              damage += rule[:additinal_damage]
+            end
           end
         end
 
         message_elements = []
-        message_elements << self.class.make_command_text(dice_count, border, additional_damage_rule)
+        message_elements << self.class.make_command_text(dice_count, border, additional_damage_rules)
         message_elements << dices.join(',')
         message_elements << "クリティカル #{critical_hit_count}" if critical_hit_count > 0
         message_elements << "ヒット数 #{hit_count}" unless hit_count.nil?
@@ -72,26 +80,22 @@ module BCDice
         end
       end
 
-      def self.make_command_text(dice_count, border, additional_damage_rule)
+      def self.make_command_text(dice_count, border, additional_damage_rules)
         elements = []
         elements << "#{dice_count}attack"
         elements << "<=#{border}" unless border.nil?
-        elements << additional_damage_rule unless additional_damage_rule.nil?
+        elements << additional_damage_rules unless additional_damage_rules.nil?
 
         "(#{elements.join('')})"
       end
 
-      def self.parse_additional_damage_rule(source)
-        m = /^\[(>?=)(\d+):\+(\d+)\]$/.match(source)
-
-        comparer = m[1]
-        comparing_target = m[2].to_i
-        additinal_damage = m[3].to_i
-
-        {
-          condition: make_additional_damage_condition(comparer, comparing_target),
-          additinal_damage: additinal_damage,
-        }
+      def self.parse_additional_damage_rules(source)
+        source.scan(/\[(>?=)(\d+):\+(\d+)\]/).map do |matched|
+          {
+            condition: make_additional_damage_condition(matched[0], matched[1].to_i),
+            additinal_damage: matched[2].to_i,
+          }
+        end
       end
 
       def self.make_additional_damage_condition(comparer, comparing_target)
