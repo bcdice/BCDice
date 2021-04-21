d792e12eb070fe3d9cf2c6374a37798797f35f6b
ViVi <vivi.shark.tech+github@gmail.com>
2021-04-21 17:17:49 +0900

    [Revulture] とりあえずファイルを用意
    現時点ではシステム名表示用。

---
+++ lib/bcdice/game_system/Revulture.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module BCDice
+  module GameSystem
+    class Revulture < Base
+      # ゲームシステムの識別子
+      ID = 'Revulture'
+
+      # ゲームシステム名
+      NAME = '光砕のリヴァルチャー'
+
+      # ゲームシステム名の読みがな
+      SORT_KEY = 'こうさいのりうあるちやあ'
+
+      # ダイスボットの使い方
+      HELP_MESSAGE = "※このダイスボットは部屋のシステム名表示用となります。\n"
+    end
+  end
+end
