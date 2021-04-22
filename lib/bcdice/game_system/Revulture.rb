f613a8143ae447ae9fd68c9f34c02abc034866cc
SAKATA Sinji <ysakasin@gmail.com>
2021-04-22 20:12:33 +0900

    Rubyっぽく手直し

--- lib/bcdice/game_system/Revulture.rb
+++ lib/bcdice/game_system/Revulture.rb
@@ -47,54 +47,61 @@ module BCDice
         end
       end
 
+      private
+
       def roll_attack(dice_count_expression, border_expression, additional_damage_rules)
         dice_count = Arithmetic.eval(dice_count_expression, round_type: RoundType::FLOOR)
+        border = Arithmetic.eval(border_expression, round_type: RoundType::FLOOR).clamp(1, 6) if border_expression
 
-        border = border_expression.nil? ? nil : Arithmetic.eval(border_expression, round_type: RoundType::FLOOR).clamp(1, 6)
+        command = make_command_text(dice_count, border, additional_damage_rules)
 
-        dices = dice_count > 0 && !(!additional_damage_rules.nil? && border.nil?) ? @randomizer.roll_barabara(dice_count, 6).sort : []
-        critical_hit_count = dices.count { |dice| dice == 1 }
-        hit_count = dices.empty? || border.nil? ? nil : dices.count { |dice| dice <= border.to_i } + critical_hit_count
-        damage = dices.empty? || additional_damage_rules.nil? ? nil : hit_count.to_i
-
-        if !damage.nil? && !additional_damage_rules.nil?
-          self.class.parse_additional_damage_rules(additional_damage_rules).each do |rule|
-            if rule[:condition].call(hit_count)
-              damage += rule[:additinal_damage]
-            end
-          end
+        if dice_count <= 0
+          return "#{command} ＞ ダイス数が 0 です"
+        elsif border.nil? && additional_damage_rules
+          return "#{command} ＞ 目標値が指定されていないため、追加ダメージを算出できません"
         end
+
+        dices = @randomizer.roll_barabara(dice_count, 6).sort
+
+        critical_hit_count = dices.count(1)
+        hit_count = dices.count { |dice| dice <= border } + critical_hit_count if border
+        damage = calc_damage(hit_count, additional_damage_rules)
 
         message_elements = []
-        message_elements << self.class.make_command_text(dice_count, border, additional_damage_rules)
-        if dice_count.zero?
-          message_elements << 'ダイス数が 0 です'
-        elsif !additional_damage_rules.nil? && border.nil?
-          message_elements << '目標値が指定されていないため、追加ダメージを算出できません'
-        else
-          message_elements << dices.join(',')
-          message_elements << "クリティカル #{critical_hit_count}" if critical_hit_count > 0
-          message_elements << "ヒット数 #{hit_count}" unless hit_count.nil?
-          message_elements << "ダメージ #{damage}" unless damage.nil?
-        end
+        message_elements << command
+        message_elements << dices.join(',')
+        message_elements << "クリティカル #{critical_hit_count}" if critical_hit_count > 0
+        message_elements << "ヒット数 #{hit_count}" if hit_count
+        message_elements << "ダメージ #{damage}" if damage
 
         Result.new(message_elements.join(' ＞ ')).tap do |r|
-          r.success = !hit_count.nil? && hit_count > 0
-          r.failure = !hit_count.nil? && hit_count == 0
+          r.condition = hit_count > 0 if hit_count
           r.critical = critical_hit_count > 0
         end
       end
 
-      def self.make_command_text(dice_count, border, additional_damage_rules)
-        elements = []
-        elements << "#{dice_count}attack"
-        elements << "<=#{border}" unless border.nil?
-        elements << additional_damage_rules unless additional_damage_rules.nil?
+      def make_command_text(dice_count, border, additional_damage_rules)
+        command = "#{dice_count}attack"
+        command += "<=#{border}" if border
+        command += additional_damage_rules if additional_damage_rules
 
-        "(#{elements.join('')})"
+        "(#{command})"
       end
 
-      def self.parse_additional_damage_rules(source)
+      def calc_damage(hit_count, additional_damage_rules)
+        return nil unless additional_damage_rules
+
+        damage = hit_count
+        parse_additional_damage_rules(additional_damage_rules).each do |rule|
+          if rule[:condition].call(hit_count)
+            damage += rule[:additinal_damage]
+          end
+        end
+
+        damage
+      end
+
+      def parse_additional_damage_rules(source)
         source.scan(/\[(>?=)(\d+):\+(\d+)\]/).map do |matched|
           {
             condition: make_additional_damage_condition(matched[0], matched[1].to_i),
@@ -103,7 +110,7 @@ module BCDice
         end
       end
 
-      def self.make_additional_damage_condition(comparer, comparing_target)
+      def make_additional_damage_condition(comparer, comparing_target)
         case comparer
         when '='
           lambda { |hit_count| hit_count == comparing_target }
