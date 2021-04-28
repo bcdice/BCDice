27e2b18e4cd736e2d95c002008e61d4625160014
GenKuzumochi <61915576+GenKuzumochi@users.noreply.github.com>
2021-04-29 00:57:30 +0900

    [コード:レイヤード]Resultに対応 (#429)
    * Resultに対応
    * 目標値0(>=0)の表示を訂正
    * リファクタリング
    
    * failureの重複を訂正

--- lib/bcdice/game_system/CodeLayerd.rb
+++ lib/bcdice/game_system/CodeLayerd.rb
@@ -30,7 +30,7 @@ module BCDice
           デフォルトダイス：10面
       MESSAGETEXT
 
-      register_prefix('[+-]?\d*CL([+-]\d+)?[@\d]*')
+      register_prefix('[+-]?\d*CL')
 
       def initialize(command)
         super(command)
@@ -41,61 +41,61 @@ module BCDice
       def eval_game_system_specific_command(command)
         debug('eval_game_system_specific_command command', command)
 
-        result = ''
+        m = /([+-]?\d+)?CL([+-]\d+)?(@(\d))?(\[(\d+)\])?([+-]\d+)?(>=(\d+))?/i.match(command)
+        return nil unless m
 
-        case command
-        when /([+-]?\d+)?CL([+-]\d+)?(@(\d))?(\[(\d+)\])?([+-]\d+)?(>=(\d+))?/i
-          m = Regexp.last_match
-          base = (m[1] || 1).to_i
-          modifier1 = m[2].to_i
-          target = (m[4] || 6).to_i
-          critical_target = (m[6] || 1).to_i
-          modifier2 = m[7].to_i
-          diff = m[9].to_i
-          result = check_roll(base, target, critical_target, diff, modifier1 + modifier2)
-        end
-
-        return nil if result.empty?
-
-        return "#{command} ＞ #{result}"
+        base = (m[1] || 1).to_i
+        modifier1 = m[2].to_i
+        target = (m[4] || 6).to_i
+        critical_target = (m[6] || 1).to_i
+        modifier2 = m[7].to_i
+        diff = m[9].to_i if m[9]
+        check_roll(command, base, target, critical_target, diff, modifier1 + modifier2)
       end
 
-      def check_roll(base, target, critical_target, diff, modifier)
+      def check_roll(command, base, target, critical_target, diff, modifier)
         if base <= 0 # クリティカルしない1D
           critical_target = 0
           base = 1
         end
+        result = Result.new
 
         target = 10 if target > 10
         dice_list = @randomizer.roll_barabara(base, 10).sort
         success_count = dice_list.count { |x| x <= target }
         critical_count = dice_list.count { |x| x <= critical_target }
+        result.critical = critical_count > 0
         success_total = success_count + critical_count + modifier
 
         mod_text = Format.modifier(modifier)
 
         # (10d10+5)
-        result = "(#{base}d10#{mod_text}) ＞ [#{dice_list.join(',')}]#{mod_text} ＞ "
-        result += "判定値[#{target}] " unless target == 6
-        result += "クリティカル値[#{critical_target}] " unless critical_target == 1
-        result += "達成値[#{success_count}]"
+        text = "#{command} ＞ (#{base}d10#{mod_text}) ＞ [#{dice_list.join(',')}]#{mod_text} ＞ "
+        text += "判定値[#{target}] " unless target == 6
+        text += "クリティカル値[#{critical_target}] " unless critical_target == 1
+        text += "達成値[#{success_count}]"
 
-        return "#{result} ＞ ファンブル！" if success_count <= 0
+        if success_count <= 0
+          result.fumble = true
+          result.failure = true
+          result.text = "#{text} ＞ ファンブル！"
+          return result
+        end
 
-        result += "+クリティカル[#{critical_count}]" if critical_count > 0
-        result += mod_text
-        result += "=[#{success_total}]" if critical_count > 0 || modifier != 0
+        text += "+クリティカル[#{critical_count}]" if result.critical?
+        text += mod_text
+        text += "=[#{success_total}]" if result.critical? || modifier != 0
 
-        success_text =
-          if diff == 0
-            success_total.to_s
-          elsif success_total >= diff
-            "成功"
-          else
-            "失敗"
-          end
-
-        return "#{result} ＞ #{success_text}"
+        if diff.nil?
+          result.text = "#{text} ＞ #{success_total}"
+        elsif success_total >= diff
+          result.text = "#{text} ＞ 成功"
+          result.success = true
+        else
+          result.text = "#{text} ＞ 失敗"
+          result.failure = true
+        end
+        return result
       end
     end
   end
