354b74a9386ffee3e15c7e32999f38a0385d1cea
SAKATA Sinji <ysakasin@gmail.com>
2021-05-27 23:39:44 +0900

    無茶ぶり表をリファクタリング

--- lib/bcdice/game_system/beginning_idol/muchaburi_table.rb
+++ lib/bcdice/game_system/beginning_idol/muchaburi_table.rb
@@ -5,19 +5,18 @@ module BCDice
     class BeginningIdol < Base
       private
 
-      def roll_muchaburi_table(command)
-        case command
-        when "LUR"
-          title = "地方アイドル無茶ぶり表"
-          table1 = [
+      MUCHABURI_TABLE = {
+        "LUR" => D6TwiceTable.new(
+          "地方アイドル無茶ぶり表",
+          [
             "地元の商店街で",
             "マスコットキャラクターと",
             "地元のプールで",
             "地元の小学校で",
             "地元のショッピングモールで",
             "田んぼの真ん中で",
-          ]
-          table2 = [
+          ],
+          [
             "愛について叫ぶ",
             "民謡を歌う",
             "ファッションショー",
@@ -25,19 +24,18 @@ module BCDice
             "ネット配信",
             "お祭り騒ぎ",
           ]
-          return textFrom1D6Table(title, table1, table2)
-
-        when "SUR"
-          title = "情熱の夏無茶ぶり表"
-          table1 = [
+        ),
+        "SUR" => D6TwiceTable.new(
+          "情熱の夏無茶ぶり表",
+          [
             "海水浴場で",
             "偉い人の前で",
             "あの有名アイドルの前で",
             "仲間の前で",
             "カメラの前で",
             "一般客の前で",
-          ]
-          table2 = [
+          ],
+          [
             "かき氷いっき食い",
             "ナンパ",
             "スイカ割り",
@@ -45,19 +43,18 @@ module BCDice
             "満面の笑顔",
             "喧嘩のふり",
           ]
-          return textFrom1D6Table(title, table1, table2)
-
-        when "WUR"
-          title = "ぬくもりの冬無茶ぶり表"
-          table1 = [
+        ),
+        "WUR" => D6TwiceTable.new(
+          "ぬくもりの冬無茶ぶり表",
+          [
             "クリスマスツリーの前で",
             "子供たちの前で",
             "大雪の中で",
             "雪が降り始めた街で",
             "暖かい部屋の中で",
             "暖房が効きすぎの部屋の中で",
-          ]
-          table2 = [
+          ],
+          [
             "雪かき",
             "アイスを食べる",
             "薄着で登場",
@@ -65,19 +62,18 @@ module BCDice
             "サンタのコスプレ",
             "おでんを急いで食べる",
           ]
-          return textFrom1D6Table(title, table1, table2)
-
-        when "NUR"
-          title = "大自然無茶ぶり表"
-          table1 = [
+        ),
+        "NUR" => D6TwiceTable.new(
+          "大自然無茶ぶり表",
+          [
             "斧を持って",
             "クワを持って",
             "釣竿を持って",
             "虫取り網を持って",
             "栄養ドリンクの宣伝をしながら",
             "命綱をつけて",
-          ]
-          table2 = [
+          ],
+          [
             "木を倒す",
             "畑を耕す",
             "昆虫採集",
@@ -85,19 +81,18 @@ module BCDice
             "一晩過ごす",
             "崖を登る",
           ]
-          return textFrom1D6Table(title, table1, table2)
-
-        when "GUR"
-          title = "聖デトワール女学園無茶ぶり表"
-          table1 = [
+        ),
+        "GUR" => D6TwiceTable.new(
+          "聖デトワール女学園無茶ぶり表",
+          [
             "裏山で",
             "食堂で",
             "先輩の前で",
             "全国放送で",
             "全校生徒の前で",
             "学園の様子を伝えるネット中継で",
-          ]
-          table2 = [
+          ],
+          [
             "歌を披露",
             "乗馬",
             "テニス",
@@ -105,19 +100,18 @@ module BCDice
             "「アイドルとは何か」を語る",
             "「アイドルをやっていてよかった瞬間」を語る",
           ]
-          return textFrom1D6Table(title, table1, table2)
-
-        when "BUR"
-          title = "アカデミー無茶ぶり表"
-          table1 = [
+        ),
+        "BUR" => D6TwiceTable.new(
+          "アカデミー無茶ぶり表",
+          [
             "TVカメラの前で",
             "ライバルと一緒に",
             "試験で",
             "寮で",
             "幼年部で",
             "初等部で",
-          ]
-          table2 = [
+          ],
+          [
             "反省会",
             "ゲリラライブ",
             "宿題をこなす",
@@ -125,10 +119,11 @@ module BCDice
             "自作の歌を披露",
             "自作のポエムを披露",
           ]
-          return textFrom1D6Table(title, table1, table2)
-        end
+        ),
+      }.freeze
 
-        return nil
+      def roll_muchaburi_table(command)
+        MUCHABURI_TABLE[command]&.roll(@randomizer)
       end
     end
   end
