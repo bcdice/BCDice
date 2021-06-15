4299b0fd4dd77e4fa2ca9070e5c2c57a2d39c4de
sakura40 <76465234+sakura40@users.noreply.github.com>
2021-06-15 20:13:24 +0900

    [AngelGear]Result対応 (#474)
    * リザルトを使用
    
    * Refactor
    
    Co-authored-by: SAKATA Sinji <ysakasin@gmail.com>

--- lib/bcdice/game_system/AngelGear.rb
+++ lib/bcdice/game_system/AngelGear.rb
@@ -48,17 +48,22 @@ module BCDice
         dice_text = dice.join(",")
         modify_n = 0
         success = 0
-
         if skill_value
           success = dice.count { |val| val <= skill_value }
           modify_n = Arithmetic.eval(modify, RoundType::FLOOR) unless modify.empty?
         end
 
-        output = "(#{command}) ＞ #{success}[#{dice_text}]#{format('%+d', modify_n)} ＞ 成功数: #{success + modify_n}"
+        gospel = '(福音発生)' if success + modify_n >= 100
+
+        output = "(#{command}) ＞ #{success}[#{dice_text}]#{format('%+d', modify_n)} ＞ 成功数: #{success + modify_n}#{gospel}"
+
         if success + modify_n >= 100
-          output += "(福音発生)"
+          Result.critical(output)
+        elsif 0 < success + modify_n
+          Result.success(output)
+        else
+          Result.failure(output)
         end
-        return output
       end
 
       TABLES = {
