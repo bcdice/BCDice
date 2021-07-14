e22abc3e2eef18e37ca49271f4f8c58445d6f206
SAKATA Sinji <ysakasin@gmail.com>
2021-07-14 22:17:23 +0900

    [PersonaO] リファクタリング

--- lib/bcdice/game_system/PersonaO.rb
+++ lib/bcdice/game_system/PersonaO.rb
@@ -28,64 +28,53 @@ module BCDice
       INFO_MESSAGE_TEXT
 
       register_prefix(
-        'PT(\-?\d+)?(@(\-?\d+))?',
-        '(\d+)PD\+(\-?\d+)%(\-?\d+)\-(\d+)'
+        'PT',
+        '\d+PD'
       )
 
       def eval_game_system_specific_command(command)
-        case command
-        when /PT(-?\d+)?(@(-?\d+))?/i
-          success_rate = Regexp.last_match(1).to_i
-          critical_border_text = Regexp.last_match(3)
-          critical_border = get_critical_border(critical_border_text, success_rate)
+        roll_attack(command) || roll_damage(command)
+      end
 
-          return attack(success_rate, critical_border)
+      private
 
-        when /(\d+)PD\+(-?\d+)%(-?\d+)-(\d+)/i
-          dice = Regexp.last_match(1).to_i
-          kotei = Regexp.last_match(2).to_i
-          hosei = Regexp.last_match(3).to_i
-          bougyo = Regexp.last_match(4).to_i
-          return damage(dice, kotei, hosei, bougyo)
+      def roll_attack(command)
+        m = /^PT(-?\d+)?(@(-?\d+))?$/i.match(command)
+        unless m
+          return nil
         end
 
-        return nil
-      end
+        success_rate = m[1].to_i
+        critical_border = m[3]&.to_i || 5
 
-      def get_critical_border(critical_border_text, _success_rate)
-        return critical_border_text.to_i unless critical_border_text.nil?
-
-        critical_border = 5
-        return critical_border
-      end
-
-      def attack(success_rate, critical_border)
         dice_value = @randomizer.roll_once(100)
-        result = get_check_result(dice_value, success_rate, critical_border)
+        result =
+          if dice_value <= critical_border
+            "クリティカル"
+          elsif dice_value <= success_rate
+            "成功"
+          else
+            "失敗"
+          end
 
-        text = "D100<=#{success_rate}@#{critical_border} ＞ #{dice_value} ＞ #{result}"
-        return text
+        return "D100<=#{success_rate}@#{critical_border} ＞ #{dice_value} ＞ #{result}"
       end
 
-      def get_check_result(dice_value, success_rate, critical_border)
-        return "クリティカル" if dice_value <= critical_border
-        return "成功" if dice_value <= success_rate
+      def roll_damage(command)
+        m = /^(\d+)PD\+(-?\d+)%(-?\d+)-(\d+)$/i.match(command)
+        unless m
+          return nil
+        end
 
-        return "失敗"
-      end
+        dice = m[1].to_i
+        kotei = m[2].to_i
+        hosei = m[3].to_i
+        bougyo = m[4].to_i
 
-      def damage(dice, kotei, hosei, bougyo)
         dice_list = @randomizer.roll_barabara(dice, 10)
         dice_sum = dice_list.sum
 
-        dice_sumF = dice_sum.to_f
-        koteiF = kotei.to_f
-        hoseiF = hosei.to_f
-        bougyoF = bougyo.to_f
-
-        inhoseiF = (hoseiF / 100) * koteiF
-        dmgF = dice_sumF + inhoseiF - bougyoF
-        dmg = dmgF.to_i
+        dmg = dice_sum + (hosei * kotei / 100.0).to_i - bougyo
 
         return "#{dice}D10+#{kotei}＊#{hosei}%-#{bougyo} ＞ [#{dice_list.join(',')}]+#{kotei}＊#{hosei}%-#{bougyo} ＞ #{dmg} ダメージ！"
       end
