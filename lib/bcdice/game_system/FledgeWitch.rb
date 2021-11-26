0cc4e347508fe741e407d8caba6a6dfecbec53d0
ViVi <vivi.shark.tech+github@gmail.com>
2021-11-26 20:25:16 +0900

    [FledgeWitch] 判定の特定の出目を特別に成功数２と見做す振る舞いを追加
    魔法スキル「ひらめいた！」（ルールブック p11 ）が該当する。

--- lib/bcdice/game_system/FledgeWitch.rb
+++ lib/bcdice/game_system/FledgeWitch.rb
@@ -32,6 +32,12 @@ module BCDice
         r: 必要なダイス目（ 1 以上 6 以下）
         例） 5fw>=4#3*6
         　　 ダイス５個、成功ライン４、必要な成功数３かつ、６のダイス目が必要
+
+        □ 6 以外の特定のダイス目を成功数 2 として扱う（ B6 か FW の後に、 &t ）
+        t: 成功数 2 として扱うダイス目
+        例） 3b6&5>=4#2
+        　　 ダイス３個、成功ライン４、必要な成功数２で、５の出目も成功数２とする
+        　　 ※魔法スキル「ひらめいた！」の効果を想定
       HELP
 
       def initialize(command)
@@ -40,23 +46,23 @@ module BCDice
         @sort_barabara_dice = true
       end
 
-      JUDGE_ROLL_REG = /^(\d+(\+\d+)*)(B6?|FW)((>=|=>)(\d+([+\-]\d+)*))?(#(\d+(\+\d+)*)(\*([1-6]))?)?$/i.freeze
-      register_prefix('(\d+(\+\d+)*)(B6?|FW)((>=|=>)(\d+([+\-]\d+)*))?(#(\d+(\+\d+)*)(\*([1-6]))?)?')
+      JUDGE_ROLL_REG = /^(\d+(\+\d+)*)(B6?|FW)(&([1-6]))?((>=|=>)(\d+([+\-]\d+)*))?(#(\d+(\+\d+)*)(\*([1-6]))?)?$/i.freeze
+      register_prefix('(\d+(\+\d+)*)(B6?|FW)(&([1-6]))?((>=|=>)(\d+([+\-]\d+)*))?(#(\d+(\+\d+)*)(\*([1-6]))?)?')
 
       def eval_game_system_specific_command(command)
         if (m = JUDGE_ROLL_REG.match(command))
-          dice_count_expression, _, keyword, _, _, success_line_expression, _, _, required_success_count_expression, _, _, required_number_expression = m.captures
+          dice_count_expression, _, keyword, _, number_as_twice_expression, _, _, success_line_expression, _, _, required_success_count_expression, _, _, required_number_expression = m.captures
 
           # 汎用コマンドの xB6 と完全に同じ書式なら、判定コマンドとして扱わない.
-          return nil if success_line_expression.nil? && required_success_count_expression.nil? && keyword != 'FW'
+          return nil if number_as_twice_expression.nil? && success_line_expression.nil? && required_success_count_expression.nil? && keyword != 'FW'
 
-          roll_judge(dice_count_expression, success_line_expression, required_success_count_expression, required_number_expression)
+          roll_judge(dice_count_expression, number_as_twice_expression, success_line_expression, required_success_count_expression, required_number_expression)
         end
       end
 
       private
 
-      def roll_judge(dice_count_expression, success_line_expression, required_success_count_expression, required_number_expression)
+      def roll_judge(dice_count_expression, number_as_twice_expression, success_line_expression, required_success_count_expression, required_number_expression)
         dice_count = Arithmetic.eval(dice_count_expression, RoundType::FLOOR)
 
         # 「成功ライン」（ルールブック p29 ）
@@ -68,16 +74,21 @@ module BCDice
         # 特定のダイス目を必要とするケース（ルールブック p59 ）における、そのダイス目
         required_number = required_number_expression ? required_number_expression.to_i : nil
 
+        # 成功数をふたつ分として数えるダイス目（ルールブック p11, 魔法スキル「ひらめいた！」）
+        number_as_twice = number_as_twice_expression ? number_as_twice_expression.to_i : nil
+
         return 'ダイス数は 1 個以上でなければなりません' if dice_count < 1
 
-        command_text = make_command_text(dice_count, success_line, required_success_count, required_number)
+        command_text = make_command_text(dice_count, number_as_twice, success_line, required_success_count, required_number)
 
         dices = @randomizer.roll_barabara(dice_count, 6).sort
 
         success_count = dices.sum do |dice|
           if dice >= success_line
-            if dice == 6
-              2 # 「６」の目なら成功数ふたつ分（ p29 ）
+            if dice == 6 || dice == number_as_twice
+              # 「６」または特別に指定された値と一致するダイス目なら、成功数ふたつ分
+              # （「６」についてはルールブック p29 、特別に値が指定されるケースはルールブック p11 ）
+              2
             else
               1
             end
@@ -96,7 +107,7 @@ module BCDice
 
         message_elements = []
         message_elements << command_text
-        message_elements << make_dices_text(dices, required_number)
+        message_elements << make_dices_text(dices, number_as_twice, success_line, required_number)
         message_elements << "成功数: #{success_count}"
         if is_special
           message_elements << "スペシャル"
@@ -111,21 +122,21 @@ module BCDice
         end
       end
 
-      def make_command_text(dice_count, success_line, required_success_count, required_number)
+      def make_command_text(dice_count, number_as_twice, success_line, required_success_count, required_number)
         command = "#{dice_count}B6"
+        command = "#{command}&#{number_as_twice}" unless number_as_twice.nil?
         command = "#{command}>=#{success_line}"
         command = "#{command}\##{required_success_count}" unless required_success_count.nil?
         command = "#{command}*#{required_number}" unless required_number.nil?
         "(#{command})"
       end
 
-      def make_dices_text(dices, required_number)
+      def make_dices_text(dices, number_as_twice, success_line, required_number)
         text = dices.map do |dice|
-          if dice == required_number
-            "*#{dice}*" # 特定のダイス目が必要とされているなら、その目は強調する
-          else
-            dice.to_s
-          end
+          dice_text = dice.to_s
+          dice_text = "*#{dice_text}*" if dice == required_number # 特定のダイス目が必要とされているなら、その目は強調する
+          dice_text = "&#{dice_text}" if dice >= success_line && dice == number_as_twice # 特別に成功数２と扱うダイス目なら、その目は強調する
+          dice_text
         end.join(',')
 
         "[#{text}]"
