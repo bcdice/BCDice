c1cf337a1ea65208fdefaa783bd0a57693570122
Hideyo Mikisato <h.mikisato@gmail.com>
2022-11-27 00:34:30 +0900

    コンヴィクタードライブ仮作成

---
+++ lib/bcdice/game_system/ConvictorDrive.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'bcdice/base'
+
+module BCDice
+  module GameSystem
+    class ConvictorDrive < Base
+      # ゲームシステムの識別子
+      ID = 'ConvictorDrive'
+
+      # ゲームシステム名
+      NAME = 'コンヴィクター・ドライブ'
+
+      # ゲームシステム名の読みがな
+      #
+      # 「ゲームシステム名の読みがなの設定方法」（docs/dicebot_sort_key.md）を参考にして
+      # 設定してください
+      SORT_KEY = 'こんういくたあとらいふ'
+
+      # ダイスボットの使い方
+      HELP_MESSAGE = <<~MESSAGETEXT
+        xCD@z>=y x個の10面ダイスで目標値y、クリティカルラインzの判定を行う
+      MESSAGETEXT
+
+      # ダイスボットで使用するコマンドを配列で列挙する
+      register_prefix("[-+*0-9\(\)]*CD")
+
+      def initialize(command)
+        super(command)
+
+        @sides_implicit_d = 10
+      end
+
+      def eval_game_system_specific_command(command)
+        debug("eval_game_system_specific_command Begin")
+
+        parser = Command::Parser.new('CD', round_type: round_type)
+                                .has_prefix_number
+                                .enable_critical
+                                .restrict_cmp_op_to(:>=)
+        cmd = parser.parse(command)
+
+        unless cmd
+          return nil
+        end
+
+        dice_list = @randomizer.roll_barabara(cmd.prefix_number, 10)
+        critical = (cmd.critical || 10).clamp(cmd.target_number, 10)
+        succeed_num = dice_list.count { |x| x >= cmd.target_number }
+        critical_num = dice_list.count { |x| x >= critical }
+
+        text = [
+          cmd.to_s,
+          dice_list.join(','),
+          "クリティカル数#{critical_num}",
+          "成功数#{succeed_num + critical_num}",
+        ].join(" ＞ ")
+
+        return Result.new.tap do |r|
+          r.critical = critical_num > 0
+          r.text = text
+        end
+      end
+    end
+  end
+end
