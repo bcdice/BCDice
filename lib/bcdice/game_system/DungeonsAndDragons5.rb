bc09457e62b89687a022d021e938464179cdd460
Faceless192x <faceless1920@yahoo.co.jp>
2023-01-22 02:51:49 +0900

    ダイス修正に対応。ACコマンドの書式を変更した。

--- lib/bcdice/game_system/DungeonsAndDragons5.rb
+++ lib/bcdice/game_system/DungeonsAndDragons5.rb
@@ -14,48 +14,70 @@ module BCDice
 
       # ダイスボットの使い方
       HELP_MESSAGE = <<~INFO_MESSAGE_TEXT
-        ・命中判定　[x]AC(敵AC)
-        　x：有利(+)・不利(-)。省略可。
+        ・命中判定　[x]AC[PN](敵AC)
+        　x：+-修正。省略可。
+        　P:有利, N:不利。省略可。
         　ファンブル／失敗／成功／クリティカル を自動判定。
-        　例）AC10 +AC18 -AC16
+        　例）AC10 +5AC18 -3AC16 ACP10 +5ACP18 -3ACP16 ACN10 +5ACN18 -3ACN16
       INFO_MESSAGE_TEXT
 
-      register_prefix('[+-]?AC\d+')
+      register_prefix('([+-]?\d+)?AC[PN]?\d+')
 
       def initialize(command)
         super(command)
 
-        @sort_barabara_dice = false # バラバラロール（Bコマンド）でソート有
+        @sort_barabara_dice = false # バラバラロール（Bコマンド）でソート無
       end
 
       def eval_game_system_specific_command(command)
         attack_roll(command)
       end
 
+      def number_with_sign_from_int(number)
+        if number == 0
+          return ""
+        elsif number > 0
+          return "+#{number}"
+        else
+          return number.to_s
+        end
+      end
+
       def attack_roll(command)
-        m = /^([-+]?)AC(\d+)(\s|$)/.match(command)
+        m = /^([-+]?\d+)?AC([PN]?)(\d+)(\s|$)/.match(command)
         unless m
           return nil
         end
 
-        advantage = m[1]
-        difficulty = m[2]&.to_i
+        modify = m[1].to_i
+        advantage = m[2]
+        difficulty = m[3].to_i
 
         usedie = 0
-        output = ["(#{advantage}AC#{difficulty})"]
+        roll_die = ""
+        output = ["(#{number_with_sign_from_int(modify)}AC#{advantage}#{difficulty})"]
 
         if advantage.empty?
           usedie = @randomizer.roll_once(20)
+          roll_die = usedie
         else
-          dices = @randomizer.roll_barabara(2, 20)
-          output.push(dices.join(","))
-          if advantage == "+"
-            usedie = dices.max
+          dice = @randomizer.roll_barabara(2, 20)
+          roll_die = "[" + dice.join(",") + "]"
+          if advantage == "P"
+            usedie = dice.max
           else
-            usedie = dices.min
+            usedie = dice.min
           end
         end
-        output.push(usedie)
+        if modify != 0
+          output.push("#{roll_die}#{number_with_sign_from_int(modify)}")
+          output.push((usedie + modify).to_s)
+        else
+          unless advantage.empty?
+            output.push(roll_die)
+          end
+          output.push(usedie.to_s)
+        end
 
         result = Result.new
         if usedie == 20
@@ -65,7 +87,7 @@ module BCDice
         elsif usedie == 1
           result.fumble = true
           output.push("ファンブル")
-        elsif usedie >= difficulty
+        elsif usedie + modify >= difficulty
           result.success = true
           output.push("成功")
         else
