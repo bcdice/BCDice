dc0aa9ae3848f283d1e3ff4ffd1ce944a86d01d7
Hideyo Mikisato <h.mikisato@gmail.com>
2023-03-05 21:56:31 +0900

    [SRS] `@`、`#` 記法に対応、および内部処理リファクタリング (#605)
    * SRSのリファクタリング
    
    - `@`, `#` によるクリティカル・ファンブルの指定を追加
    - [c,f]方式によるクリティカル・ファンブルの指定を正規表現をなるべく使わない方法に変更
    
    * SRSベースのシステムのテスト追加
    
    * ヘルプメッセージのテスト修正漏れ
    
    * デフォルトのダイスコマンドへのフォールバック条件をより厳密に
    
    修正値だけがある場合でもデフォルトダイスにフォールバックする
    
    * フォールバック判定を小文字も考慮させる

--- lib/bcdice/game_system/SRS.rb
+++ lib/bcdice/game_system/SRS.rb
@@ -16,27 +16,33 @@ module BCDice
 
       HELP_MESSAGE_1 = <<~HELP_MESSAGE
         ・判定
-        　・通常判定：2D6+m>=t[c,f]
+        　・通常判定：2D6+m@c#f>=t または 2D6+m>=t[c,f]
         　　修正値m、目標値t、クリティカル値c、ファンブル値fで判定ロールを行います。
-        　　修正値、クリティカル値、ファンブル値は省略可能です（[]ごと省略可）。
+        　　修正値、クリティカル値、ファンブル値は省略可能です（[]ごと省略可、@c・#fの指定は順不同）。
         　　クリティカル値、ファンブル値の既定値は、それぞれ12、2です。
         　　自動成功、自動失敗、成功、失敗を自動表示します。
 
         　　例) 2d6>=10　　　　　修正値0、目標値10で判定
         　　例) 2d6+2>=10　　　　修正値+2、目標値10で判定
         　　例) 2d6+2>=10[11]　　↑をクリティカル値11で判定
+        　　例) 2d6+2@11>=10 　　↑をクリティカル値11で判定
         　　例) 2d6+2>=10[12,4]　↑をクリティカル値12、ファンブル値4で判定
+        　　例) 2d6+2@12#4>=10 　↑をクリティカル値12、ファンブル値4で判定
+        　　例) 2d6+2>=10[,4]　　↑をクリティカル値12、ファンブル値4で判定（クリティカル値の省略）
+        　　例) 2d6+2#4>=10　　　↑をクリティカル値12、ファンブル値4で判定（クリティカル値の省略）
       HELP_MESSAGE
 
       HELP_MESSAGE_2 = <<~HELP_MESSAGE
-        　・クリティカルおよびファンブルのみの判定：2D6+m[c,f]
+        　・クリティカルおよびファンブルのみの判定：2D6+m@c#f または 2D6+m[c,f]
         　　目標値を指定せず、修正値m、クリティカル値c、ファンブル値fで判定ロールを行います。
-        　　修正値、クリティカル値、ファンブル値は省略可能です（[]は省略不可）。
+        　　修正値、クリティカル値、ファンブル値は省略可能です（[]は省略不可、@c・#fの指定は順不同）。
         　　自動成功、自動失敗を自動表示します。
 
         　　例) 2d6[]　　　　修正値0、クリティカル値12、ファンブル値2で判定
         　　例) 2d6+2[11]　　修正値+2、クリティカル値11、ファンブル値2で判定
+        　　例) 2d6+2@11 　　修正値+2、クリティカル値11、ファンブル値2で判定
         　　例) 2d6+2[12,4]　修正値+2、クリティカル値12、ファンブル値4で判定
+        　　例) 2d6+2@12#4 　修正値+2、クリティカル値12、ファンブル値4で判定
       HELP_MESSAGE
 
       HELP_MESSAGE_3 = <<~HELP_MESSAGE
@@ -50,17 +56,13 @@ module BCDice
 
       # 成功判定のエイリアスコマンド定義用のクラスメソッドを提供するモジュール
       module ClassMethods
+        # 成功判定のエイリアスコマンドの一覧
+        # @return [Array<String>]
+        attr_reader :aliases
+
         # ダイスボットの説明文を返す
         # @return [String]
         attr_reader :help_message
-
-        # 目標値あり成功判定のエイリアスコマンドの正規表現を返す
-        # @return [Regexp, nil]
-        attr_reader :aliases_re_for_srs_roll_with_target_value
-
-        # 目標値なし成功判定のエイリアスコマンドの正規表現を返す
-        # @return [Regexp, nil]
-        attr_reader :aliases_re_for_srs_roll_without_target_value
 
         # 成功判定のエイリアスコマンドを設定する
         # @param [String] aliases エイリアスコマンド（可変長引数）
@@ -70,71 +72,48 @@ module BCDice
         # 実行時にそれが2D6に置換されるようになる。
         def set_aliases_for_srs_roll(*aliases)
           aliases_upcase = aliases.map(&:upcase)
-          aliases_part = aliases_upcase
-                         .map { |a| Regexp.escape(a) }
-                         .join('|')
 
-          @aliases_re_for_srs_roll_with_target_value = Regexp.new(
-            '\A' \
-            "(?:#{aliases_part})" \
-            '((?:[-+][-+\d]+)?>=\d+(?:\[\d*(?:,\d+)?\])?)\z'
-          )
-
-          @aliases_re_for_srs_roll_without_target_value = Regexp.new(
-            '\A' \
-            "(?:#{aliases_part})" \
-            '([-+][-+\d]+)?(\[\d*(?:,\d+)?\])?\z'
-          )
-
-          prepare_help_msg_for_aliases_for_srs_roll(aliases_upcase)
-          @help_message = concatenate_help_messages
-
+          @aliases = aliases_upcase.map { |a| Regexp.escape(a) }
+          @help_message = concatenate_help_messages(aliases_upcase)
           return self
         end
 
         # 成功判定のエイリアスコマンドを未設定にする
         # @return [self]
         def clear_aliases_for_srs_roll
-          @aliases_re_for_srs_roll_with_target_value = nil
-          @aliases_re_for_srs_roll_without_target_value = nil
+          @aliases = []
           @help_message = SRS::DEFAULT_HELP_MESSAGE
-
           return self
         end
 
         private
 
-        # 成功判定のエイリアスコマンドの説明文を用意する
+        # ダイスボットの説明文を結合する
         # @param [Array<String>] aliases エイリアスコマンドの配列
-        # @return [self]
+        # @return [String] 結合された説明文
         # @todo 現在は2文字のエイリアスコマンドに幅を合わせてある。
         #   エイリアスコマンドの文字数が変わる場合があれば、位置を調整するコードが
         #   必要。
-        def prepare_help_msg_for_aliases_for_srs_roll(aliases)
-          @help_msg_for_aliases_for_srs_roll_with_target_value =
+        def concatenate_help_messages(aliases)
+          help_msg_for_aliases_for_target_value =
             aliases
-            .map { |a| "　　例) #{a}+2>=10　　　　 2d6+2>=10と同じ（#{a}が2D6のショートカットコマンド）\n" }
+            .map do |a|
+              "　　例) #{a}+2>=10　　　　 2d6+2>=10と同じ（#{a}が2D6のショートカットコマンド）\n"
+            end
             .join()
-
-          @help_msg_for_aliases_for_srs_roll_without_target_value =
+          help_msg_for_aliases_for_without_target_value =
             aliases
             .map do |a|
               "　　例) #{a}　　　　　 2d6[]と同じ（#{a}が2D6のショートカットコマンド）\n" \
-              "　　例) #{a}+2[12,4]　 2d6+2[12,4]と同じ（#{a}が2D6のショートカットコマンド）\n"
+              "　　例) #{a}+2@12#4　　2d6+2@12#4と同じ（#{a}が2D6のショートカットコマンド）\n"
             end
             .join()
 
-          return self
-        end
-
-        # ダイスボットの説明文を結合する
-        # @return [String] 結合された説明文
-        def concatenate_help_messages
-          "#{SRS::HELP_MESSAGE_1}" \
-            "#{@help_msg_for_aliases_for_srs_roll_with_target_value}\n" \
-            "#{SRS::HELP_MESSAGE_2}" \
-            "#{@help_msg_for_aliases_for_srs_roll_without_target_value}\n" \
-            "#{SRS::HELP_MESSAGE_3}"
+          return "#{SRS::HELP_MESSAGE_1}" \
+                 "#{help_msg_for_aliases_for_target_value}\n" \
+                 "#{SRS::HELP_MESSAGE_2}" \
+                 "#{help_msg_for_aliases_for_without_target_value}\n" \
+                 "#{SRS::HELP_MESSAGE_3}"
         end
       end
 
@@ -153,16 +132,10 @@ module BCDice
           DEFAULT_HELP_MESSAGE
         end
 
-        # 目標値あり成功判定のエイリアスコマンドの正規表現を返す
-        # @return [nil]
-        def aliases_re_for_srs_roll_with_target_value
-          nil
-        end
-
-        # 目標値なし成功判定のエイリアスコマンドの正規表現を返す
-        # @return [nil]
-        def aliases_re_for_srs_roll_without_target_value
-          nil
+        # 成功判定のエイリアスコマンドの一覧
+        # @return [Array<String>]
+        def aliases
+          []
         end
       end
 
@@ -187,21 +160,16 @@ module BCDice
         self.class.help_message
       end
 
+      # 成功判定のエイリアスコマンドの一覧
+      # @return [Array<String>]
+      def aliases
+        self.class.aliases
+      end
+
       # 既定のクリティカル値
       DEFAULT_CRITICAL_VALUE = 12
       # 既定のファンブル値
       DEFAULT_FUMBLE_VALUE = 2
-
-      # 目標値あり成功判定の正規表現
-      SRS_ROLL_WITH_TARGET_VALUE_RE =
-        /\A2D6([-+][-+\d]+)?>=(\d+)(?:\[(\d+)?(?:,(\d+))?\])?\z/.freeze
-      # 目標値なし成功判定の正規表現
-      SRS_ROLL_WITHOUT_TARGET_VALUE_RE =
-        /\A2D6([-+][-+\d]+)?\[(\d+)?(?:,(\d+))?\]\z/.freeze
-
-      # 既定の閾値指定表記
-      SRS_ROLL_DEFAULT_THRESHOLDS =
-        "[#{DEFAULT_CRITICAL_VALUE},#{DEFAULT_FUMBLE_VALUE}]"
 
       # 成功判定コマンドのノード
       SRSRollNode = Struct.new(
@@ -221,9 +189,15 @@ module BCDice
       # @param [String] command 入力されたコマンド
       # @return [Result, nil] ダイスロールコマンドの実行結果
       def eval_game_system_specific_command(command)
-        alias_replaced_with_2d6 = replace_alias_for_srs_roll_with_2d6(command)
+        legacy_c_f_match = /(.+)\[(.*)\]\z/.match(command)
+        node =
+          if legacy_c_f_match
+            parse_legacy(legacy_c_f_match[1], legacy_c_f_match[2])
+          else
+            parse(command)
+          end
 
-        if (node = parse(alias_replaced_with_2d6))
+        if node
           return execute_srs_roll(node)
         end
 
@@ -232,67 +206,55 @@ module BCDice
 
       private
 
-      # 成功判定のエイリアスコマンドを2D6に置換する
-      # @param [String] input 入力文字列
-      # @return [String]
-      def replace_alias_for_srs_roll_with_2d6(input)
-        case input
-        when self.class.aliases_re_for_srs_roll_with_target_value
-          return "2D6#{Regexp.last_match(1)}"
-        when self.class.aliases_re_for_srs_roll_without_target_value
-          modifier = Regexp.last_match(1)
-          thresholds = Regexp.last_match(2) || SRS_ROLL_DEFAULT_THRESHOLDS
-
-          return "2D6#{modifier}#{thresholds}"
-        else
-          return input
-        end
-      end
-
-      # 構文解析する
-      # @param [String] command コマンド文字列
-      # @return [SRSRollNode, nil]
       def parse(command)
-        case command
-        when SRS_ROLL_WITH_TARGET_VALUE_RE
-          return parse_srs_roll_with_target_value(Regexp.last_match)
-        when SRS_ROLL_WITHOUT_TARGET_VALUE_RE
-          return parse_srs_roll_without_target_value(Regexp.last_match)
-        else
+        prefix_re = Regexp.new(["2D6"].concat(aliases()).join('|'), Regexp::IGNORECASE)
+        parser = Command::Parser.new(prefix_re, round_type: @round_type)
+                                .enable_critical
+                                .enable_fumble
+                                .restrict_cmp_op_to(nil, :>=)
+        cmd = parser.parse(command)
+        unless cmd
           return nil
         end
+
+        if command.start_with?(/2D6/i) && cmd.critical.nil? && cmd.fumble.nil? && cmd.target_number.nil?
+          # fallback to default dice
+          return nil
+        end
+
+        cmd.critical ||= DEFAULT_CRITICAL_VALUE
+        cmd.fumble ||= DEFAULT_FUMBLE_VALUE
+
+        return SRSRollNode.new(cmd.modify_number, cmd.critical, cmd.fumble, cmd.target_number)
       end
 
-      # 修正値を評価する
-      # @param [String, nil] modifier_str 修正値部分の文字列
-      # @return [Integer] 修正値
-      def eval_modifier(modifier_str)
-        return 0 unless modifier_str
+      def parse_legacy(command, c_f)
+        parsed_c_f = c_f.split(",")
+        critical, fumble = case parsed_c_f.length
+                           when 0
+                             [DEFAULT_CRITICAL_VALUE, DEFAULT_FUMBLE_VALUE]
+                           when 1
+                             [Integer(parsed_c_f[0], exception: false), DEFAULT_FUMBLE_VALUE]
+                           when 2
+                             if parsed_c_f[0] == ""
+                               [DEFAULT_CRITICAL_VALUE, Integer(parsed_c_f[1], exception: false)]
+                             else
+                               [Integer(parsed_c_f[0], exception: false), Integer(parsed_c_f[1], exception: false)]
+                             end
+                           end
+        if critical.nil? || fumble.nil?
+          return nil
+        end
 
-        return ArithmeticEvaluator.eval(modifier_str, round_type: @round_type)
-      end
+        prefix_re = Regexp.new(["2D6"].concat(aliases()).join('|'), Regexp::IGNORECASE)
+        parser = Command::Parser.new(prefix_re, round_type: @round_type)
+                                .restrict_cmp_op_to(nil, :>=)
+        cmd = parser.parse(command)
+        unless cmd
+          return nil
+        end
 
-      # 目標値あり成功判定の正規表現マッチ情報からノードを作る
-      # @param [MatchData] m 正規表現のマッチ情報
-      # @return [SRSRollNode]
-      def parse_srs_roll_with_target_value(m)
-        modifier = eval_modifier(m[1])
-        target_value = m[2].to_i
-        critical_value = m[3]&.to_i || DEFAULT_CRITICAL_VALUE
-        fumble_value = m[4]&.to_i || DEFAULT_FUMBLE_VALUE
-
-        return SRSRollNode.new(modifier, critical_value, fumble_value, target_value)
-      end
-
-      # 目標値なし成功判定の正規表現マッチ情報からノードを作る
-      # @param [MatchData] m 正規表現のマッチ情報
-      # @return [SRSRollNode]
-      def parse_srs_roll_without_target_value(m)
-        modifier = eval_modifier(m[1])
-        critical_value = m[2]&.to_i || DEFAULT_CRITICAL_VALUE
-        fumble_value = m[3]&.to_i || DEFAULT_FUMBLE_VALUE
-
-        return SRSRollNode.new(modifier, critical_value, fumble_value, nil)
+        return SRSRollNode.new(cmd.modify_number, critical, fumble, cmd.target_number)
       end
 
       # 成功判定を実行する
