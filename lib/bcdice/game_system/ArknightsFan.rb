e0b958c7c73c8177d5743537fd94e067980639e1
Ayase00 <sinzyo.zer07@gmail.com>
2024-01-02 20:57:55 +0900

    Refined.

--- lib/bcdice/game_system/ArknightsFan.rb
+++ lib/bcdice/game_system/ArknightsFan.rb
@@ -11,20 +11,20 @@ module BCDice
       SORT_KEY = "あーくないつTRPG"
 
       HELP_MESSAGE = <<~TEXT
-        ■ 判定 (nD100>=x)
-          nD100のダイスロールをして、x 以下であれば成功。
+        ■ 判定 (nADm>=x)
+          nDmのダイスロールをして、x 以下であれば成功。
           x が91以上でエラー。
           x が10以下でクリティカル。
 
-        ■ 判定 (nB100>=x)
-          nB100のダイスロールをして、
+        ■ 判定 (nABm>=x)
+          nBmのダイスロールをして、
             x 以下であれば成功数+1。
             x が91以上でエラー。成功数+1。
             x が10以下でクリティカル。成功数-1。
           上記による成功数をカウント。
 
-        ■ 判定 (nB100>=x--役職)
-          nB100のダイスロールをして、
+        ■ 判定 (nBm>=x--役職)
+          nBmのダイスロールをして、
             x 以下であれば成功数+1。
             x が91以上でエラー。成功数+1。
             x が10以下でクリティカル。成功数-1。
@@ -78,6 +78,10 @@ module BCDice
           return roll_B_withtype(command, $1.to_i, 100, $2.to_i, $3)
         when /^(\d+)AB(\d+)<=(\d+)--([^\d\s]+)$/
           return roll_B_withtype(command, $1.to_i, $2.to_i, $3.to_i, $4)
+        when /^(\d+)AB<=(\d+)--([^\d\s]+)0$/
+          return roll_B(command, $1.to_i, 100, $2.to_i)
+        when /^(\d+)AB(\d+)<=(\d+)--([^\d\s]+)0$/
+          return roll_B(command, $1.to_i, $2.to_i, $3.to_i)
         when /^--ADDICTION$/
           return roll_addiction(command, TABLES)
         when /^--WORSENING$/
@@ -89,34 +93,9 @@ module BCDice
 
       private
 
-      def roll_addiction(command, tables)
-        syndrome = roll_ark_tables(command, tables, @randomizer)
-        return nil unless syndrome
-        return syndrome
-      end
-
-      def roll_worsening(command, tables)
-        randomizer = @randomizer
-        addiction = roll_ark_tables(command, tables, randomizer)
-        return nil unless addiction
-        elapse = randomizer.roll_barabara(1, 6)[0] + 1
-        
-        return "#{addiction}: #{elapse} rounds"
-      end
-
-      def roll_ark_tables(command, tables, randomizer)
-        table = tables[command]
-        return nil unless table
-
-        contents = table[2]
-        dice_result = randomizer.roll_barabara(1, table[1].to_i)[0]
-        return contents[dice_result-1]
-      end
-
       def roll_D(command, times, sides, target)
         dice_list = @randomizer.roll_barabara(times, sides).sort
         total = dice_list.sum
-
         success = total <= target
 
         crierror = 
@@ -151,27 +130,25 @@ module BCDice
       end
 
       def roll_B(command, times, sides, target)
-        result = process_B(times, sides, target)
-        dice_list = result[0]
-        success_count = result[1]
+        dice_list, success_count, critical_count, error_count = process_B(times, sides, target)
+        result_count = success_count + critical_count - error_count
 
-        return "(#{command}) ＞ [#{dice_list.join(',')}] ＞ 成功数#{success_count}"
+        return "(#{command}) ＞ [#{dice_list.join(',')}] ＞ #{success_count}+#{critical_count}C-#{error_count}E ＞ 成功数#{result_count}"
       end
 
       def roll_B_withtype(command, times, sides, target, type)
-        result = process_B(times, sides, target)
-        dice_list = result[0]
-        success_count = result[1]
+        dice_list, success_count, critical_count, error_count = process_B(times, sides, target)
+        result_count = success_count + critical_count - error_count
 
         type_effect = 
-          if (type == "SNIPER") && (success_count > 0)
+          if (type == "SNIPER") && (result_count > 0)
             1
           else
             0
           end
-        success_count += type_effect
+          result_count += type_effect
 
-        return "(#{command}) ＞ [#{dice_list.join(',')}] ＞ 役職効果(#{type})+#{type_effect} ＞ 成功数#{success_count}"
+        return "(#{command}) ＞ [#{dice_list.join(',')}] ＞ #{success_count}+#{critical_count}C-#{error_count}E+#{type_effect}(#{type}) ＞ 成功数#{result_count}"
       end
 
       def process_B(times, sides, target)
@@ -192,11 +169,34 @@ module BCDice
           error_count += dice_list.count(num)
         end
 
-        success_count += critical_count
-        success_count -= error_count
-
         return [dice_list, success_count, critical_count, error_count]
 
+      end
+
+      ### ダイステーブル（ユーザー定義配列）の処理
+
+      def roll_addiction(command, tables)
+        syndrome = roll_ark_tables(command, tables, @randomizer)
+        return nil unless syndrome
+        return syndrome
+      end
+
+      def roll_worsening(command, tables)
+        randomizer = @randomizer
+        addiction = roll_ark_tables(command, tables, randomizer)
+        return nil unless addiction
+        elapse = randomizer.roll_barabara(1, 6)[0] + 1
+        
+        return "#{addiction}: #{elapse} rounds"
+      end
+
+      def roll_ark_tables(command, tables, randomizer)
+        table = tables[command]
+        return nil unless table
+
+        contents = table[2]
+        dice_result = randomizer.roll_barabara(1, table[1].to_i)[0]
+        return contents[dice_result-1]
       end
 
       def roll_b_type_old(command)
