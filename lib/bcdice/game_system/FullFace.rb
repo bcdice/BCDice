8cf84ae115f190a7984fe14a28dff75492561f38
Faceless192x <faceless1920@yahoo.co.jp>
2024-02-16 13:53:50 +0900

    一般判定の成否と戦闘判定のダメージ計算を実装

--- lib/bcdice/game_system/FullFace.rb
+++ lib/bcdice/game_system/FullFace.rb
@@ -14,11 +14,13 @@ module BCDice
 
       # ダイスボットの使い方
       HELP_MESSAGE = <<~INFO_MESSAGETEXT
-        ■判定　x+bFF<=t   x:ヒート(省略時は3) b:判定修正 t:能力値
+        ■判定　x+bFF<=[a[,t]]d   x:ヒート(省略時は3) b:判定修正 a:能力値 t:難易度(省略可) d:基本ダメージ(省略可)
 
-        例)FF<=2:   能力値2で判定し、その結果(成功数,1の目の数,バースト)を表示。
-           6FF<=3:  ヒート6,能力値3で戦闘判定し、その結果( 〃 )を表示。
-           8+2FF<=3:ヒート8,判定修正+2,能力値3で戦闘判定し、その結果( 〃 )を表示。
+        例)FF<=[2]:   能力値2で判定し、その結果(成功数,1の目の数,6の目の数,バースト)を表示。
+           6FF<=[3]:  ヒート6,能力値3で戦闘判定し、その結果( 〃 )を表示。
+           8+2FF<=[3]:ヒート8,判定修正+2,能力値3で戦闘判定し、その結果( 〃 )を表示。
+           FF<=[2,1]: 能力値2,難易度1で判定し、その結果(成功数,1の目の数,6の目の数,成功・失敗,バースト)を表示。
+           6FF<=[3]1:  ヒート6,能力値3で戦闘判定し、その結果(成功数,1の目の数,6の目の数,ダメージ,バースト)を表示。
       INFO_MESSAGETEXT
 
       register_prefix('([+\d]+)*FF')
@@ -33,13 +35,15 @@ module BCDice
       # @param [String] command
       # @return [Result]
       def resolute_action(command)
-        m = /^(\d*)([+\d]+)*FF<=(\d)$/.match(command)
+        m = /^(\d*)([+\d]+)*FF<=\[(\d)(,(\d))?\](\d)?$/.match(command)
         return nil unless m
 
         heat_level = m[1].to_i
         heat_level = 3 if heat_level == 0
         modify = Arithmetic.eval("0#{m[2]}", @round_type)
         status_no = m[3].to_i
+        target_no = m[5].to_i
+        damage_no = m[6].to_i
 
         dice_array = []
 
@@ -66,6 +70,7 @@ module BCDice
         end
 
         return Result.new.tap do |result|
+          command_out = "(#{heat_level}#{Format.modifier(modify)}FF<=[#{status_no}]"
           if sixs >= 2
             result.fumble = true
             result.condition = false
@@ -76,10 +81,27 @@ module BCDice
           result_txt = []
           result_txt.push("成功度(#{success_num})")
           result_txt.push("1の目(#{ones_total})") if ones_total > 0
+          result_txt.push("6の目(#{sixs})") if sixs > 0
+          if target_no > 0
+            command_out = command_out.chop + ",#{target_no}]"
+            if success_num >= target_no
+              result_txt.push("成功")
+              result.condition = true
+            else
+              result_txt.push("失敗")
+              result.condition = false
+            end
+          end
+          if damage_no > 0
+            command_out += damage_no.to_s
+            damage = damage_no + ones_total
+            result_txt.push("ダメージ(#{damage})")
+          end
           result_txt.push("バースト") if result.fumble?
+          command_out += ")"
 
           sequence = [
-            "(#{heat_level}#{Format.modifier(modify)}FF<=#{status_no})",
+            command_out,
             dice_array.join('+').to_s,
             result_txt.join(',').to_s,
           ].compact
