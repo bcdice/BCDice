6edb0d1e99b15fb6d9b0d2a1fb72ce948aa13b98
Faceless192x <faceless1920@yahoo.co.jp>
2024-02-17 21:36:44 +0900

    FFコマンドの書式を戻し、FAコマンドとFBコマンドに分けて新設した

--- lib/bcdice/game_system/FullFace.rb
+++ lib/bcdice/game_system/FullFace.rb
@@ -14,28 +14,172 @@ module BCDice
 
       # ダイスボットの使い方
       HELP_MESSAGE = <<~INFO_MESSAGETEXT
-        ■判定　x+bFF<=[a[,t]]d   x:ヒート(省略時は3) b:判定修正 a:能力値 t:難易度(省略可) d:基本ダメージ(省略可)
+        ■判定　x+bFF<=a   x:ヒート(省略時は3) b:判定修正 a:能力値
 
-        例)FF<=[2]:   能力値2で判定し、その結果(成功数,1の目の数,6の目の数,バースト)を表示。
-           6FF<=[3]:  ヒート6,能力値3で戦闘判定し、その結果( 〃 )を表示。
-           8+2FF<=[3]:ヒート8,判定修正+2,能力値3で戦闘判定し、その結果( 〃 )を表示。
-           FF<=[2,1]: 能力値2,難易度1で判定し、その結果(成功数,1の目の数,6の目の数,成功・失敗,バースト)を表示。
-           6FF<=[3]1:  ヒート6,能力値3で戦闘判定し、その結果(成功数,1の目の数,6の目の数,ダメージ,バースト)を表示。
+        例)FF<=2:   能力値2で判定し、その結果(成功数,1の目の数,6の目の数,バースト)を表示。
+           6FF<=3:  ヒート6,能力値3で戦闘判定し、その結果( 〃 )を表示。
+           8+2FF<=3:ヒート8,判定修正+2,能力値3で戦闘判定し、その結果( 〃 )を表示。
+
+        ■能力値判定　x+bFA<=[a,t]   x:ヒート(省略時は3) b:判定修正 a:能力値 t:難易度(省略可)
+
+        例)FA<=[2]:   能力値2で判定し、その結果(成功数,1の目の数,6の目の数,バースト)を表示。
+           FA<=[2,1]: 能力値2,難易度1で判定し、その結果(成功数,1の目の数,6の目の数,成功・失敗,バースト)を表示。
+
+        ■戦闘判定　x+bFB<=[a,t]d   x:ヒート(省略時は3) b:判定修正 a:能力値 t:難易度 d:基本ダメージ(省略可)
+
+        例)6FB<=[3]1:  ヒート6,能力値3,基本ダメージ1で戦闘判定し、その結果(成功数,1の目の数,6の目の数,ダメージ,バースト)を表示。
+           8+2FB<=[3]2:ヒート8,判定修正+2,能力値3,基本ダメージ1で戦闘判定し、その結果( 〃 )を表示。
+
       INFO_MESSAGETEXT
 
-      register_prefix('([+\d]+)*FF')
+      register_prefix('([+\d]+)*FF', '([+\d]+)*FA', '([+\d]+)*FB')
 
       def eval_game_system_specific_command(command)
-        resolute_action(command)
+        resolute_action(command) || resolute_ability_action(command) || resolute_battle(command)
       end
 
       private
 
-      # 技能判定
+      # 判定
       # @param [String] command
       # @return [Result]
       def resolute_action(command)
-        m = /^(\d*)([+\d]+)*FF<=\[(\d)(,(\d))?\](\d)?$/.match(command)
+        m = /^(\d*)([+\d]+)*FF<=(\d)$/.match(command)
+        return nil unless m
+
+        heat_level = m[1].to_i
+        heat_level = 3 if heat_level == 0
+        modify = Arithmetic.eval("0#{m[2]}", @round_type)
+        status_no = m[3].to_i
+
+        dice_array = []
+
+        dice = @randomizer.roll_barabara(heat_level, 6)
+        ones = dice.count(1)
+        sixs = dice.count(6)
+        success_num = dice.count { |val| val <= status_no }
+        dice_array.push(dice.join(","))
+
+        if modify > 0
+          dice = @randomizer.roll_barabara(modify, 6)
+          ones += dice.count(1)
+          success_num += dice.count { |val| val <= status_no }
+          dice_array.push(dice.join(","))
+        end
+        ones_total = ones
+
+        while ones > 0
+          dice = @randomizer.roll_barabara(ones, 6)
+          ones = dice.count(1)
+          ones_total += ones
+          success_num += dice.count { |val| val <= status_no }
+          dice_array.push(dice.join(","))
+        end
+
+        return Result.new.tap do |result|
+          command_out = "(#{heat_level}#{Format.modifier(modify)}FF<=#{status_no}"
+          if sixs >= 2
+            result.fumble = true
+            result.condition = false
+          else
+            result.condition = (success_num > 0)
+            result.critical = (ones_total > 0)
+          end
+          result_txt = []
+          result_txt.push("成功度(#{success_num})")
+          result_txt.push("1の目(#{ones_total})") if ones_total > 0
+          result_txt.push("6の目(#{sixs})") if sixs > 0
+          result_txt.push("バースト") if result.fumble?
+          command_out += ")"
+
+          sequence = [
+            command_out,
+            dice_array.join('+').to_s,
+            result_txt.join(',').to_s,
+          ].compact
+
+          result.text = sequence.join(" ＞ ")
+        end
+      end
+
+      # 能力値判定
+      # @param [String] command
+      # @return [Result]
+      def resolute_ability_action(command)
+        m = /^(\d*)([+\d]+)*FA<=\[(\d)(,(\d))?\]$/.match(command)
+        return nil unless m
+
+        heat_level = m[1].to_i
+        heat_level = 3 if heat_level == 0
+        modify = Arithmetic.eval("0#{m[2]}", @round_type)
+        status_no = m[3].to_i
+        target_no = m[5].to_i
+
+        dice_array = []
+
+        dice = @randomizer.roll_barabara(heat_level, 6)
+        ones = dice.count(1)
+        sixs = dice.count(6)
+        success_num = dice.count { |val| val <= status_no }
+        dice_array.push(dice.join(","))
+
+        if modify > 0
+          dice = @randomizer.roll_barabara(modify, 6)
+          ones += dice.count(1)
+          success_num += dice.count { |val| val <= status_no }
+          dice_array.push(dice.join(","))
+        end
+        ones_total = ones
+
+        while ones > 0
+          dice = @randomizer.roll_barabara(ones, 6)
+          ones = dice.count(1)
+          ones_total += ones
+          success_num += dice.count { |val| val <= status_no }
+          dice_array.push(dice.join(","))
+        end
+
+        return Result.new.tap do |result|
+          command_out = "(#{heat_level}#{Format.modifier(modify)}FA<=[#{status_no}]"
+          if sixs >= 2
+            result.fumble = true
+            result.condition = false
+          else
+            result.condition = (success_num > 0)
+            result.critical = (ones_total > 0)
+          end
+          result_txt = []
+          result_txt.push("成功度(#{success_num})")
+          result_txt.push("1の目(#{ones_total})") if ones_total > 0
+          result_txt.push("6の目(#{sixs})") if sixs > 0
+          if target_no > 0
+            command_out = command_out.chop + ",#{target_no}]"
+            if success_num >= target_no
+              result_txt.push("成功")
+              result.condition = true
+            else
+              result_txt.push("失敗")
+              result.condition = false
+            end
+          end
+          result_txt.push("バースト") if result.fumble?
+          command_out += ")"
+
+          sequence = [
+            command_out,
+            dice_array.join('+').to_s,
+            result_txt.join(',').to_s,
+          ].compact
+
+          result.text = sequence.join(" ＞ ")
+        end
+      end
+
+      # 戦闘判定
+      # @param [String] command
+      # @return [Result]
+      def resolute_battle(command)
+        m = /^(\d*)([+\d]+)*FB<=\[(\d)(,(\d))?\](\d)?$/.match(command)
         return nil unless m
 
         heat_level = m[1].to_i
@@ -70,7 +214,7 @@ module BCDice
         end
 
         return Result.new.tap do |result|
-          command_out = "(#{heat_level}#{Format.modifier(modify)}FF<=[#{status_no}]"
+          command_out = "(#{heat_level}#{Format.modifier(modify)}FB<=[#{status_no}]"
           if sixs >= 2
             result.fumble = true
             result.condition = false
