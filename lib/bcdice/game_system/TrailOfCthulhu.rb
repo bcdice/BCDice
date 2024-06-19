1ee3af9ebf63f7f03e7f17613867accd6d0799b5
Faceless192x <faceless1920@yahoo.co.jp>
2024-06-19 09:51:59 +0900

    トレイル・オブ・クトゥルーの実装(セレクタ表示用)

---
+++ lib/bcdice/game_system/TrailOfCthulhu.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module BCDice
+  module GameSystem
+    class TrailOfCthulhu < Base
+      # ゲームシステムの識別子
+      ID = 'TrailOfCthulhu'
+
+      # ゲームシステム名
+      NAME = 'トレイル・オブ・クトゥルー'
+
+      # ゲームシステム名の読みがな
+      SORT_KEY = 'とれいるおふくとうるう'
+
+      # ダイスボットの使い方
+      HELP_MESSAGE = "※このダイスボットは部屋のシステム名表示用となります。\n"
+    end
+  end
+end
