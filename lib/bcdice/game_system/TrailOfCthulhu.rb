9378245d13558e58c1714008c0de33ee0f22f080
Faceless192x <faceless1920@yahoo.co.jp>
2024-06-24 01:44:40 +0900

    技能判定コマンドの実装

---
+++ lib/bcdice/game_system/TrailOfCthulhu.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module BCDice
+  module GameSystem
+    class TrailOfCthulhu < Base
+      # ゲームシステムの識別子
+      ID = 'TrailOfCthulhu'
+
+      # ゲームシステム名
+      NAME = 'トレイル・オブ・クトゥルフ'
+
+      # ゲームシステム名の読みがな
+      SORT_KEY = 'とれいるおふくとうるう'
+
+      # ダイスボットの使い方
+      HELP_MESSAGE = <<~INFO_MESSAGETEXT
+        ■技能判定　TCb>=t   b:消費プール・ポイント t:難易度
+
+        例)TC2>=5:消費プール・ポイント2,難易度5で技能判定し、その結果を表示。
+           TC>=3:  難易度3で技能判定する。
+
+      INFO_MESSAGETEXT
+
+      register_prefix("TC")
+
+      def eval_game_system_specific_command(command)
+        resolute_action(command)
+      end
+
+      private
+
+      # 技能判定
+      # @param [String] command
+      # @return [Result]
+      def resolute_action(command)
+        m = /TC(\d*)>=(\d+)/.match(command)
+        return nil unless m
+
+        bonus = m[1].to_i
+        difficulty = m[2].to_i
+
+        dice = @randomizer.roll_once(6)
+        total = dice + bonus
+
+        return Result.new.tap do |result|
+          result.condition = (total >= difficulty)
+
+          sequence = [
+            "(TC#{bonus}>=#{difficulty})",
+            "#{dice}+#{bonus}",
+            total.to_s,
+            result.success? ? "成功" : "失敗"
+          ].compact
+
+          result.text = sequence.join(" ＞ ")
+        end
+      end
+    end
+  end
+end
