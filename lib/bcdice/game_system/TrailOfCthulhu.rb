193aa52967e3703c18891d1566627f73e75d0b81
Faceless192x <faceless1920@yahoo.co.jp>
2024-06-24 08:06:51 +0900

    純粋主義モード用技能判定(難易度指定なし)を実装

--- lib/bcdice/game_system/TrailOfCthulhu.rb
+++ lib/bcdice/game_system/TrailOfCthulhu.rb
@@ -14,10 +14,12 @@ module BCDice
 
       # ダイスボットの使い方
       HELP_MESSAGE = <<~INFO_MESSAGETEXT
-        ■技能判定　TCb>=t   b:消費プール・ポイント t:難易度
+        ■技能判定　TCb[>=t]   b:消費プール・ポイント t:難易度(省略可能)
 
         例)TC2>=5:消費プール・ポイント2,難易度5で技能判定し、その結果を表示。
-           TC>=3:  難易度3で技能判定する。
+           TC>=3: 難易度3で技能判定する。
+           TC:    難易度指定せずに技能判定する。
+           TC3:   消費プール・ポイント3,難易度指定せずに技能判定する。
 
       INFO_MESSAGETEXT
 
@@ -33,24 +35,32 @@ module BCDice
       # @param [String] command
       # @return [Result]
       def resolute_action(command)
-        m = /TC(\d*)>=(\d+)/.match(command)
+        m = /TC([+\d]*)(>=(\d+))?/.match(command)
         return nil unless m
 
         bonus = m[1].to_i
-        difficulty = m[2].to_i
+        difficulty = m[3].to_i
 
         dice = @randomizer.roll_once(6)
         total = dice + bonus
 
         return Result.new.tap do |result|
-          result.condition = (total >= difficulty)
+          if difficulty > 0
+            result.condition = (total >= difficulty)
 
-          sequence = [
-            "(TC#{bonus}>=#{difficulty})",
-            "#{dice}+#{bonus}",
-            total.to_s,
-            result.success? ? "成功" : "失敗"
-          ].compact
+            sequence = [
+              "(TC#{bonus}>=#{difficulty})",
+              "#{dice}+#{bonus}",
+              total.to_s,
+              result.success? ? "成功" : "失敗"
+            ].compact
+          else
+            sequence = [
+              "(TC#{bonus})",
+              "#{dice}+#{bonus}",
+              total.to_s,
+            ].compact
+          end
 
           result.text = sequence.join(" ＞ ")
         end
