9794347ed0a671dbed11110ee1583ef7f9cc0a10
SAKATA Sinji <ysakasin@gmail.com>
2025-01-27 19:31:41 +0900

    [SkynautsBouken] i18n化の不足処理を修正

--- lib/bcdice/game_system/SkynautsBouken_Korean.rb
+++ lib/bcdice/game_system/SkynautsBouken_Korean.rb
@@ -47,72 +47,11 @@ module BCDice
         super(command)
         @locale = :ko_kr
       end
-      
+
       register_prefix_from_super_class()
 
-      # 프록시 메서드 정의
-      def tables
-        call_private_translate_tables
-      end
-      def direction_infos
-        call_private_translate_direction_infos
-      end
-
-      private
-
-      def eval_game_system_specific_command(command)
-        # 동적으로 TABLES와 DIRECTION_INFOS 사용
-        command_sn(command) || command_d(command) || command_avo(command) || command_snavo(command) ||
-          command_snd(command) || roll_tables(command, tables)
-      end
-
-      # command_d와 같은 메서드에서 direction_infos를 동적으로 호출
-      def command_d(command)
-        m = D_REGEXP.match(command)
-        return nil unless m
-
-        fire_count = m[3].to_i # 砲撃回数
-        fire_range = m[1].to_s # 砲撃範囲
-        ballistics = m[5].to_i # 《弾道学》
-
-        points = get_fire_points(fire_count, fire_range)
-        command = command.sub("SF/", "[#{translate('SkynautsBouken.big_shake')},#{translate('SkynautsBouken.fire')}]/")
-                         .sub("FS/", "[#{translate('SkynautsBouken.fire')},#{translate('SkynautsBouken.big_shake')}]/")
-                         .sub("F/", "[#{translate('SkynautsBouken.fire')}]/").sub("S/", "[#{translate('SkynautsBouken.big_shake')}]/")
-        result = ["(#{command})", get_points_text(points, 0, 0)]
-        if ballistics != 0
-          dir = direction_infos[ballistics]
-          diff_x, diff_y = dir[:position_diff]
-          result[-1] += "\n"
-          result << "《#{translate('SkynautsBouken.ballistics')}》#{dir[:name]}"
-          result << get_points_text(points, diff_x, diff_y)
-        end
-
-        result.compact.join(" ＞ ")
-      end
-
-      def command_avo(command)
-        debug("AVO", command)
-        dmg = command.match(/^AVO@([2468])(.*?)$/)
-        return nil unless dmg
-
-        dir = direction_infos[dmg[1].to_i]
-        diff_x, diff_y = dir[:position_diff]
-        "《#{translate('SkynautsBouken.avoidance')}》#{dir[:name]} ＞ " + dmg[2].gsub(/\(?\[(\d),(\d{1,2})\]\)?/) do
-          y = Regexp.last_match(1).to_i + diff_y
-          x = Regexp.last_match(2).to_i + diff_x
-          get_xy_text(x, y)
-        end
-      end
-
-      # `send` 메서드를 사용하여 부모 클래스의 private 메서드 호출
-      def call_private_translate_tables
-        self.class.send(:translate_tables, @locale)
-      end
-      def call_private_translate_direction_infos
-        self.class.send(:translate_direction_infos, @locale)
-      end
-
+      TABLES = translate_tables(:ko_kr).freeze
+      DIRECTION_INFOS = translate_direction_infos(:ko_kr).freeze
     end
   end
 end
