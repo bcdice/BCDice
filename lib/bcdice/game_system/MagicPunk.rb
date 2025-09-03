6e0185753afa5d6c96e5ab7022800fde58eab3de
uniquis <uniquis@gmail.com>
2025-09-03 22:52:41 +0900

    Pass: 1d Test

--- lib/bcdice/game_system/MagicPunk.rb
+++ lib/bcdice/game_system/MagicPunk.rb
@@ -21,10 +21,44 @@ module BCDice
         通常の判定に加えてチャレンジ値x以上の目が必要になる。
       TEXT
 
-      register_prefix()
+      register_prefix('^\d?MP\d+')
 
       def eval_game_system_specific_command(command)
-        return nil
+        return roll_mp(command)
+      end
+      private
+      def roll_mp(command)
+        m = /^(\d?)MP(\d+)$/.match(command)
+        return nil unless m
+
+        times = m[1].size > 0 ? m[1].to_i : 1
+        spec = m[2].to_i
+
+        dice_list = @randomizer.roll_barabara(times, 20)
+
+        check = dice_list.count{|d| d <= spec} > 0
+        is_bb = dice_list.all?{|d| d == 1}
+        if is_bb
+          check = false
+        end
+
+        result = if check
+          max = dice_list.select{|d| d <= spec}.max
+          if max == spec
+            "JP"
+          else
+            "成功(#{max})"
+          end
+        else
+          if is_bb
+            "BB"
+          else
+            "失敗"
+          end
+        end
+
+        return "(#{times}MP#{spec}) > [#{dice_list.join(',')}] > #{result}"
+
       end
     end
   end
