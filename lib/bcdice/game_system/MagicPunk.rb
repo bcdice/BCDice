8927c64dde41a2798e001acf2c1ad919cfc47d99
uniquis <uniquis@gmail.com>
2025-09-06 00:49:59 +0900

    impl  0d Mode

--- lib/bcdice/game_system/MagicPunk.rb
+++ lib/bcdice/game_system/MagicPunk.rb
@@ -17,8 +17,11 @@ module BCDice
         nD20のダイスロールをして、m以下の目があれば成功。
         mと同じ目があればジャックポット(自動成功)。
         すべての目が1ならバッドビート(自動失敗)。
-        ■ チャレンジ判定 (nMPm>=x、nMPmCx)
+        ■ チャレンジ判定 (nMPmCx)
         通常の判定に加えてチャレンジ値x以上の目が必要になる。
+        ■ ダイス数0 (0MPmCx)
+        修正によりダイス数が0になった場合は2d20のダイスロールを行う。
+        2つの目からより悪い結果になる方を採用する。
       TEXT
 
       register_prefix('^\d?MP\d+')
@@ -36,26 +39,32 @@ module BCDice
         opt1 = m[3]
         arg1 = m[4].to_i
 
+        is_zero = dices == 0
+        times = is_zero ? 2 : dices
         challenge = ["c", "C"].include?(opt1) ? arg1 : 0
 
-        dice_list = @randomizer.roll_barabara(dices, 20)
+        dice_list = @randomizer.roll_barabara(times, 20)
 
-        check = dice_list.any?{|d| d <= spec && challenge <= d}
-        is_jp = dice_list.any?{|d| d == spec}
-        is_bb = dice_list.all?{|d| d == 1}
+        check_method = is_zero ? :all? : :any?
+        fail_method = is_zero ? :any? : :all?
+
+        check = dice_list.public_send(check_method){|d| d <= spec && challenge <= d}
+        is_jp = dice_list.public_send(check_method){|d| d == spec}
+        is_bb = dice_list.public_send(fail_method){|d| d == 1}
 
         result = if is_bb # 自動失敗優先
           "失敗(BB)"
         elsif is_jp
           "成功(JP)"
         elsif check
-          max = dice_list.select{|d| d <= spec}.max
-          "成功(#{max})"
+          value_method = is_zero ? :min : :max
+          value = dice_list.select{|d| d <= spec}.public_send(value_method)
+          "成功(#{value})"
         else
           "失敗"
         end
 
-        return "(#{dices}MP#{spec}C#{arg1}) > [#{dice_list.join(',')}] > #{result}"
+        return "(#{dices}MP#{spec}C#{challenge}) > [#{dice_list.join(',')}] > #{result}"
 
       end
     end
