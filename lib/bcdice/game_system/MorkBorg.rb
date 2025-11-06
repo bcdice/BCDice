70b38fff78ddaea6c47d7d3dcc73229a00022bd2
faceless192x <faceless1920@yahoo.co.jp>
2025-11-06 18:29:51 +0900

    Moraleの解釈違いを修正。Broken表を追加

--- lib/bcdice/game_system/MorkBorg.rb
+++ lib/bcdice/game_system/MorkBorg.rb
@@ -24,14 +24,15 @@ module BCDice
 
         例)INS: 1d6を振って、イニシアティヴの結果を表示(PC先行を成功として表示)
 
-        ■モラル　sMOR s: 能力値(省略時:0)
+        ■モラル　sMORt s: 能力値(省略時:0) t:相手クリーチャーのモラル値
 
-        例)MOR: 1d6を振って、モラルの結果を表示(降伏を成功として表示)
+        例)MOR8: 2d6を振って、モラル判定の結果を表示(モラル崩壊を成功として表示)
 
 
         ■各種表
 
-        ・遭遇反応表(ERT)
+        ・遭遇反応表 Reaction (ERT)
+        ・破損 Broken (BRO)
 
       INFO_MESSAGETEXT
 
@@ -110,7 +111,7 @@ module BCDice
           if total >= 4
             Result.success("PCs go first")
           else
-            Result.failure("enemies go first")
+            Result.failure("Enemies go first")
           end
 
         result.text = "(#{command}) ＞ #{die}#{with_symbol(num_status)} ＞ #{total} ＞ #{result.text}"
@@ -121,21 +122,30 @@ module BCDice
       # @param [String] command
       # @return [Result]
       def resolute_morale(command)
-        m = /^([+-]?\d+)?MOR$/.match(command)
+        m = /^([+-]?\d+)?MOR(\d+)$/.match(command)
         return nil unless m
 
         num_status = m[1].to_i
+        num_target = m[2].to_i
 
-        die = @randomizer.roll_once(6)
-        total = die + num_status
+        dice_list = @randomizer.roll_barabara(2, 6)
+        dice_total = dice_list.sum()
+        total = dice_total + num_status
+
+        die = ""
         result =
-          if total >= 4
-            Result.success("surrenders")
+          if dice_total <= num_target
+              Result.failure("Morale maintenance")
           else
-            Result.failure("flees")
+            die = @randomizer.roll_once(6)
+            if die >= 4
+              Result.success("(Surrenders)")
+            else
+              Result.success("(Flees)")
+            end
           end
+        result.text = "(#{command}) ＞ #{dice_total}#{with_symbol(num_status)} ＞ #{total} ＞ #{die}#{result.text}"
 
-        result.text = "(#{command}) ＞ #{die}#{with_symbol(num_status)} ＞ #{total} ＞ #{result.text}"
         return result
       end
 
@@ -158,6 +168,17 @@ module BCDice
             'Almost friendly',
             'Helpful',
             'Helpful',
+          ]
+        ),
+
+        'BRO' => DiceTable::Table.new(
+          '崩壊表',
+          '1D4',
+          [
+            "Fall unconscious for d4 rounds, awaken with d4 HP.",
+            "Roll a d6: 1–5 = Broken or severed limb. 6 = Lost eye. Can't act for d4 rounds then become active with d4 HP.",
+            "Haemorrhage: death in d2 hours unless treated. All tests are DR16 the first hour. DR18 the last hour.",
+            "Dead.",
           ]
         ),
       }.freeze
