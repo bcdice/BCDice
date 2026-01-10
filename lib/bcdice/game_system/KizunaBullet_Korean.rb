0d717241c0de96332c7275c485a413a9a9fbdfd1
MYAKKU <yesyesoroaroa@gmail.com>
2026-01-10 18:37:29 +0900

    Remove unnecessary overrides in KizunaBullet_Korean

--- lib/bcdice/game_system/KizunaBullet_Korean.rb
+++ lib/bcdice/game_system/KizunaBullet_Korean.rb
@@ -60,7 +60,7 @@ module BCDice
         조사 표・다이나믹 ID
         조사 표・베이직과 다이나믹 IBD
 
-        해저드표 HA
+        해저드 표 HA
 
         통상 다이제스트: 당신들에게 새로운 명령이 떨어졌다(조사가 의뢰되었다).
         1:그 사건의 내용은……. NI1
@@ -95,161 +95,14 @@ module BCDice
         6:불릿은 마지막으로……HC6
       MESSAGETEXT
 
-      TABLES = translate_tables(:ko_kr) # @localeを :ko_kr に強制
-
       def initialize(command)
         super(command)
-
-        @sides_implicit_d = 6
-        @round_type = RoundType::CEIL
-        @d66_sort_type = D66SortType::NO_SORT
+        @locale = :ko_kr
       end
 
-      # translateをオーバーライドして、常に:ko_krを使うようにする
-      def translate(key, **opts)
-        super(key, locale: :ko_kr, **opts)
-      end
+      TABLES = translate_tables(:ko_kr)
 
-      def eval_game_system_specific_command(command)
-        roll_max(command) || roll_investigate(command) || roll_sedative(command) || roll_solve(command) || roll_tables(command, self.class::TABLES)
-      end
-
-      private
-
-      # 最大値
-      def roll_max(command)
-        parser = Command::Parser.new("DM", round_type: @round_type)
-                                .has_prefix_number
-        parsed = parser.parse(command)
-        unless parsed
-          return nil
-        end
-
-        # 最大値計算
-        dice_list = @randomizer.roll_barabara(parsed.prefix_number, 6)
-        max = dice_list.max
-
-        return Result.new.tap do |r|
-          # テキストを整形
-          r.text = "#{command} ＞ [#{dice_list.join(',')}] ＞ #{max}"
-        end
-      end
-
-      # 調査判定
-      def roll_investigate(command)
-        parser = Command::Parser.new("IN", round_type: @round_type)
-                                .has_prefix_number
-        parsed = parser.parse(command)
-        unless parsed
-          return nil
-        end
-
-        texts = []
-        is_success = false
-        is_fumble = false
-
-        # 最大値計算
-        dice_list = @randomizer.roll_barabara(parsed.prefix_number, 6)
-        max = dice_list.max
-
-        if max >= 5
-          # 5以上の出目があった場合
-          # 成功フラグを立てる
-          is_success = true
-          # 成功メッセージを追加
-          texts.push(translate("KizunaBullet.INVESTIGATE.success"))
-        elsif max >= 3
-          # 3以上の出目があった場合
-          # 失敗メッセージを追加
-          texts.push(translate("KizunaBullet.INVESTIGATE.failure"))
-          # ［パートナーのヘルプ］メッセージを追加
-          texts.push(translate("KizunaBullet.INVESTIGATE.partnerHelp"))
-        else
-          # 上記以外
-          # ファンブルフラグを立てる
-          is_fumble = true
-          # 失敗メッセージを追加
-          texts.push(translate("KizunaBullet.INVESTIGATE.failure"))
-          # ファンブルメッセージを追加
-          texts.push(translate("KizunaBullet.INVESTIGATE.fumble"))
-        end
-
-        return Result.new.tap do |r|
-          # テキストを整形
-          r.text = "#{command} ＞ [#{dice_list.join(',')}] ＞ #{texts.join('')}"
-          # 各種成否を格納
-          r.condition = is_success
-          r.fumble = is_fumble
-        end
-      end
-
-      # 鎮静判定
-      def roll_sedative(command)
-        parser = Command::Parser.new("SE", round_type: @round_type)
-                                .has_suffix_number
-        parsed = parser.parse(command)
-        unless parsed
-          return nil
-        end
-
-        text = ''
-        is_success = false
-
-        # 合計値計算
-        sum = @randomizer.roll_sum(2, 6)
-
-        if parsed.suffix_number > 12
-          # 目標値が12より大きい場合
-          # ［晶滅］メッセージを追加
-          text = translate("KizunaBullet.SEDATIVE.burst")
-        elsif parsed.suffix_number < 6
-          # 目標値が6より小さい場合
-          # ［生存］メッセージを追加
-          text = translate("KizunaBullet.SEDATIVE.alive")
-        elsif sum > parsed.suffix_number
-          # 合計値が目標値より大きい場合
-          # 成功フラグを立てる
-          is_success = true
-          # 成功メッセージを追加
-          text = translate("KizunaBullet.SEDATIVE.success")
-        else
-          # 上記以外
-          # ［強制鎮静］に必要な［キズナ］のチェック数の計算
-          # 目標値と出目の差分を計算
-          dif = parsed.suffix_number - sum
-          # チェック一つごとに結果に+2
-          check = (dif / 2) + 1
-          # 失敗メッセージを追加
-          text = translate("KizunaBullet.SEDATIVE.failure", check: check.to_s)
-        end
-
-        return Result.new.tap do |r|
-          # テキストを整形
-          r.text = "#{command} ＞ #{sum} ＞ #{text}"
-          # 各種成否を格納
-          r.condition = is_success
-        end
-      end
-
-      # 解決 ［アクション］のダメージと［アクシデント］のダメージ軽減
-      def roll_solve(command)
-        parser = Command::Parser.new("SO", round_type: @round_type)
-                                .enable_prefix_number
-        parsed = parser.parse(command)
-        unless parsed
-          return nil
-        end
-
-        # 合計値計算
-        sum = @randomizer.roll_sum(parsed.prefix_number.to_i + 2, 6)
-
-        return Result.new.tap do |r|
-          # テキストを整形
-          r.text = "#{command} ＞ #{sum}"
-        end
-      end
-
-      register_prefix('\d+DM', '\d+IN', 'SE\d+', '\d*SO', TABLES.keys)
+      register_prefix_from_super_class()
     end
   end
 end
