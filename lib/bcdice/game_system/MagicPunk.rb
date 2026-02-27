9b3bb242aba4deeb88b4820bfea02ab7ece0b131
MY4k <yesyesoraora@gmail.com>
2026-02-27 20:32:37 +0900

    MagicPunk:i18n化

--- lib/bcdice/game_system/MagicPunk.rb
+++ lib/bcdice/game_system/MagicPunk.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "bcdice/base"
+
 module BCDice
   module GameSystem
     class MagicPunk < Base
@@ -36,42 +38,36 @@ module BCDice
         m = /^(\d*)MP(\d+)(C?)(\d*)$/.match(command)
         return nil unless m
 
-        # 構文解析
         dices = m[1].empty? ? 1 : m[1].to_i
         spec = m[2].to_i
         opt1 = m[3]
         arg1 = m[4].to_i
 
-        # ダイス数0モードフラグ
         is_zero = dices == 0
-        # チャレンジ値
         challenge = opt1 == "C" ? arg1 : 0
 
-        # ダイスロール
         dice_list = @randomizer.roll_barabara(is_zero ? 2 : dices, 20)
 
-        # 通常は1つ成功なら成功、0ダイス時はすべて成功したとき成功
         check_method = is_zero ? :all? : :any?
-        # 通常はすべて失敗なら失敗、0ダイス時は1つ失敗したら失敗
         fail_method = is_zero ? :any? : :all?
 
-        check = dice_list.public_send(check_method) { |d| d <= spec && challenge <= d } # 通常判定
-        is_jp = dice_list.public_send(check_method) { |d| d == spec } # ジャックポット判定
-        is_bb = dice_list.public_send(fail_method) { |d| d == 1 } # バッドビート判定
+        check = dice_list.public_send(check_method) { |d| d <= spec && challenge <= d }
+        is_jp = dice_list.public_send(check_method) { |d| d == spec }
+        is_bb = dice_list.public_send(fail_method) { |d| d == 1 }
 
-        result = if is_bb # 自動失敗優先
+        result = if is_bb
                    is_jp = false
                    check = false
-                   "失敗(BB)"
+                   translate("MagicPunk.bad_beat")
                  elsif is_jp
                    check = true
-                   "成功(JP)"
+                   translate("MagicPunk.jackpot")
                  elsif check
                    value_method = is_zero ? :min : :max
                    value = dice_list.select { |d| d <= spec }.public_send(value_method)
-                   "成功(#{value})"
+                   translate("MagicPunk.success", value: value)
                  else
-                   "失敗"
+                   translate("failure")
                  end
 
         return Result.new.tap do |r|
