c6d0709c126708b49feee302b1bfd302d1222920
hikoyamamayo <hikoyama.mayo@gmail.com>
2026-04-18 19:00:30 +0900

    サプリメントITTMに対応、Robocopの行数制限を無視するように

--- lib/bcdice/game_system/PastFutureParadox.rb
+++ lib/bcdice/game_system/PastFutureParadox.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
+
 module BCDice
   module GameSystem
     class PastFutureParadox < Base
@@ -3426,3 +3428,5 @@ module BCDice
     end
   end
 end
+
+# rubocop:enable Metrics/ClassLength
